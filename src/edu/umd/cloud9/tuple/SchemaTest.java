7a58e400f6a4765da3a3e4848093a877d2269105
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2007-10-21 03:08:55 +0000

    added actual junit testcases for Schema

--- src/edu/umd/cloud9/tuple/SchemaTest.java
+++ src/edu/umd/cloud9/tuple/SchemaTest.java
@@ -1,18 +1,38 @@
 package edu.umd.cloud9.tuple;
 
+import static org.junit.Assert.assertEquals;
+import junit.framework.JUnit4TestAdapter;
+
+import org.junit.Test;
+
 public class SchemaTest {
 
-	public static final Schema MY_SCHEMA = new Schema();
+	public static final Schema SCHEMA1 = new Schema();
 	static {
-		MY_SCHEMA.addColumn("p1", String.class, "default");
-		MY_SCHEMA.addColumn("p2", Integer.class, new Integer(1));
+		SCHEMA1.addField("field1", String.class, "default");
+		SCHEMA1.addField("field2", Integer.class, new Integer(1));
 	}
-	
-	public static void main(String[] args) {
-		Tuple tuple = MY_SCHEMA.instantiate();
-		
-		System.out.println(tuple.get(0) + ", " + tuple.get(1));
-		System.out.println(tuple.get("p1") + ", " + tuple.get("p2"));
+
+	@Test
+	public void test1() {
+		Tuple tuple = SCHEMA1.instantiate();
+
+		assertEquals(tuple.get(0), "default");
+		assertEquals(tuple.get(1), new Integer(1));
+
+		assertEquals(tuple.get("field1"), "default");
+		assertEquals(tuple.get("field2"), new Integer(1));
+	}
+
+	@Test
+	public void test2() {
+		Tuple tuple = SCHEMA1.instantiate("Hello world!", new Integer(5));
+		assertEquals(tuple.get(0), "Hello world!");
+		assertEquals(tuple.get(1), new Integer(5));
+	}
+
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(SchemaTest.class);
 	}
 
 }
