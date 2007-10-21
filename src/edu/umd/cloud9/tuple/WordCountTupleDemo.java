03b9c1fcfb543b68d36985acb5dedd2475f8bbcb
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2007-10-21 18:33:35 +0000

    switched to BytesWritable representation of keys, writes SequenceFiles as output

--- src/edu/umd/cloud9/tuple/WordCountTupleDemo.java
+++ src/edu/umd/cloud9/tuple/WordCountTupleDemo.java
@@ -1,11 +1,16 @@
 package edu.umd.cloud9.tuple;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
@@ -16,23 +21,25 @@ import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.SequenceFileOutputFormat;
 
 public class WordCountTupleDemo {
 
 	public static final Schema KEY_SCHEMA = new Schema();
 	static {
-		KEY_SCHEMA.addColumn("Token", String.class, "");
-		KEY_SCHEMA.addColumn("OddOrEven", Integer.class, new Integer(1));
+		KEY_SCHEMA.addField("Token", String.class, "");
+		KEY_SCHEMA.addField("OddOrEven", Integer.class, new Integer(1));
 	}
-
+	
 	// similar to WordCount, except that it keeps track of counts in lines that
 	// are odd # chars in length or even # of chars in length --- only point is
 	// to demo how tuples might be used
 	public static class MapClass extends MapReduceBase implements Mapper {
 		private final static IntWritable one = new IntWritable(1);
-		private final static Text star = new Text("*");
-		private Text word = new Text();
-
+		
+		private BytesWritable bytekey = new BytesWritable();
+		private Tuple tuple = KEY_SCHEMA.instantiate();
+		
 		public void map(WritableComparable key, Writable value,
 				OutputCollector output, Reporter reporter) throws IOException {
 			String line = ((Text) value).toString();
@@ -40,24 +47,25 @@ public class WordCountTupleDemo {
 			while (itr.hasMoreTokens()) {
 				String token = itr.nextToken();
 
-				Tuple t = KEY_SCHEMA.instantiate();
-				t.Set(0, token);
-				t.Set(1, line.length() % 2);
-
-				word.set(t.pack());
-				output.collect(word, one);
-				output.collect(star, one);
+				tuple.set(0, token);
+				tuple.set(1, line.length() % 2);
+				byte[] bytes = tuple.pack();
+				
+				bytekey.set(bytes, 0, bytes.length);
+		
+				output.collect(bytekey, one);
 			}
 		}
 	}
 
 	public static class ReduceClass extends MapReduceBase implements Reducer {
-		public void reduce(WritableComparable key, Iterator values,
+		public synchronized void reduce(WritableComparable key, Iterator values,
 				OutputCollector output, Reporter reporter) throws IOException {
 			int sum = 0;
 			while (values.hasNext()) {
 				sum += ((IntWritable) values.next()).get();
 			}
+
 			output.collect(key, new IntWritable(sum));
 		}
 	}
@@ -66,7 +74,7 @@ public class WordCountTupleDemo {
 		String filename = "sample-input/bible+shakes.nopunc";
 		String outputPath = "sample-counts";
 		int mapTasks = 20;
-		int reduceTasks = 60;
+		int reduceTasks = 20;
 		boolean local = false;
 
 		if (local) {
@@ -77,9 +85,7 @@ public class WordCountTupleDemo {
 		JobConf conf = new JobConf(WordCountTupleDemo.class);
 		conf.setJobName("wordcount");
 
-		// the keys are words (strings)
-		conf.setOutputKeyClass(Text.class);
-		// the values are counts (ints)
+		conf.setOutputKeyClass(BytesWritable.class);
 		conf.setOutputValueClass(IntWritable.class);
 
 		conf.setMapperClass(MapClass.class);
@@ -89,6 +95,8 @@ public class WordCountTupleDemo {
 		conf.setNumMapTasks(mapTasks);
 		conf.setNumReduceTasks(reduceTasks);
 		conf.setInputPath(new Path(filename));
+		
+		conf.setOutputFormat(SequenceFileOutputFormat.class);
 		conf.setOutputPath(new Path(outputPath));
 
 		JobClient.runJob(conf);
