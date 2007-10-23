c1df3bdc374d6e048f28972df26b511a5cd03d4b
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2007-10-23 17:38:32 +0000

    extracted new wrapper class: LocalTupleRecordWriter

---
+++ src/edu/umd/cloud9/tuple/LocalTupleRecordWriter.java
@@ -0,0 +1,44 @@
+package edu.umd.cloud9.tuple;
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.mapred.JobConf;
+
+public class LocalTupleRecordWriter {
+
+	private BytesWritable mBytes = new BytesWritable();
+	private LongWritable mLong = new LongWritable();
+
+	private long mCnt = 0;
+
+	private SequenceFile.Writer writer;
+
+	public LocalTupleRecordWriter(String file) throws IOException {
+		JobConf config = new JobConf();
+
+		writer = SequenceFile.createWriter(FileSystem.get(config), config,
+				new Path(file), LongWritable.class, BytesWritable.class);
+	}
+
+	public void add(Tuple tuple) throws IOException {
+		byte[] buf = tuple.pack();
+
+		mLong.set(mCnt);
+		mBytes.set(buf, 0, buf.length);
+		writer.append(mLong, mBytes);
+		mCnt++;
+	}
+
+	public long getRecordCount() {
+		return mCnt;
+	}
+
+	public void close() throws IOException {
+		writer.close();
+	}
+}
--- src/edu/umd/cloud9/tuple/PackRecordsDemo.java
+++ src/edu/umd/cloud9/tuple/PackRecordsDemo.java
@@ -5,15 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.BytesWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.SequenceFile;
-import org.apache.hadoop.mapred.JobConf;
-
 public class PackRecordsDemo {
-
 
 	public static final Schema RECORD_SCHEMA = new Schema();
 	static {
@@ -26,51 +18,22 @@ public class PackRecordsDemo {
 		String infile = "../umd-hadoop-dist/sample-input/bible+shakes.nopunc";
 		String outfile = "../umd-hadoop-dist/sample-input/bible+shakes.nopunc.packed";
 
-		JobConf config = new JobConf();
-		
-		System.out.println(config.getWorkingDirectory());
-		
-		BytesWritable bytes = new BytesWritable();
-		LongWritable longw = new LongWritable();
-		
-		SequenceFile.Writer writer = SequenceFile.createWriter(FileSystem.get(config), config,
-				new Path(outfile), LongWritable.class, BytesWritable.class);
+		LocalTupleRecordWriter writer = new LocalTupleRecordWriter(outfile);
 
-			BufferedReader data = new BufferedReader(new InputStreamReader(
-					new FileInputStream(infile)));
+		BufferedReader data = new BufferedReader(new InputStreamReader(
+				new FileInputStream(infile)));
 
-			String line;
-			long cnt = 0;
-			while ((line = data.readLine()) != null) {
-				tuple.set(0, line);
-				byte[] buf = tuple.pack();
-				
-				longw.set(cnt);
-				bytes.set(buf, 0, buf.length);
-				writer.append(longw, bytes);
-				cnt++;
-			}
+		String line;
+		while ((line = data.readLine()) != null) {
+			tuple.set(0, line);
+			writer.add(tuple);
+		}
 
-			data.close();
-			writer.close();
+		data.close();
+		writer.close();
 
-			System.out.println("Wrote " + cnt + " records.");
+		System.out.println("Wrote " + writer.getRecordCount() + " records.");
 
 	}
-	
-	/*
-	public static void main(String[] args) throws IOException {
 
-		FileSystem fs = new RawLocalFileSystem();
-		JobConf config = new JobConf();
-		
-		System.out.println(config.getWorkingDirectory());
-		
-		SequenceFile.Writer writer = SequenceFile.createWriter(FileSystem.get(config), config,
-				new Path("test.out"), LongWritable.class, BytesWritable.class);
-
-		writer.append(new LongWritable(1), new BytesWritable());
-		
-		writer.close();
-	}*/
 }
