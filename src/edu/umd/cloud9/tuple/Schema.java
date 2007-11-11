e259928d1df7619a36dd17decaeb00e0198c943a
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2007-11-11 04:18:16 +0000

    javadoc

--- src/edu/umd/cloud9/tuple/Schema.java
+++ src/edu/umd/cloud9/tuple/Schema.java
@@ -1,3 +1,19 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package edu.umd.cloud9.tuple;
 
 import java.util.HashMap;
@@ -19,6 +35,20 @@ import org.apache.hadoop.io.Writable;
  * 	MYSCHEMA.addField(&quot;int&quot;, Integer.class, new Integer(1));
  * }
  * </pre>
+ * 
+ * <p>
+ * The following field types are allowed:
+ * </p>
+ * 
+ * <ul>
+ * <li>Boolean</li>
+ * <li>Integer</li>
+ * <li>Long</li>
+ * <li>Float</li>
+ * <li>Double</li>
+ * <li>String</li>
+ * <li>concrete classes that implement Writable</li>
+ * </ul>
  * 
  * <p>
  * Schema instances can be locked to prevent further changes. Any attempt to
