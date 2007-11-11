029d5c001b2fde0630fe45d47a4e73c1c8a9cea6
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2007-11-11 02:49:30 +0000

    new Exception type

---
+++ src/edu/umd/cloud9/tuple/SchemaException.java
@@ -0,0 +1,9 @@
+package edu.umd.cloud9.tuple;
+
+public class SchemaException extends RuntimeException {
+	public static final long serialVersionUID = 2673497691l;
+	
+	public SchemaException(String message) {
+		super(message);
+	}
+}
---
+++ src/edu/umd/cloud9/tuple/TupleException.java
@@ -0,0 +1,9 @@
+package edu.umd.cloud9.tuple;
+
+public class TupleException extends RuntimeException {
+	public static final long serialVersionUID = 640927654842l;
+	
+	public TupleException(String message) {
+		super(message);
+	}
+}
