3a098957e62adb717673bbe7070cc27e343bd75c
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-02-05 21:43:20 +0000

    prints more graph info

--- src/edu/umd/cloud9/exercise/pagerank/SequentialPageRank.java
+++ src/edu/umd/cloud9/exercise/pagerank/SequentialPageRank.java
@@ -16,11 +16,13 @@ import edu.uci.ics.jung.graph.Graph;
 public class SequentialPageRank {
 
 	public static void main(String[] args) throws IOException {
-		if ( args.length != 1 ) {
-			System.err.println("usage: SequentialPageRage [graph-adjacency-list]");
+		if (args.length != 2) {
+			System.err
+					.println("usage: SequentialPageRage [graph-adjacency-list] [damping-factor]");
 			System.exit(-1);
 		}
 		String infile = args[0];
+		float alpha = Float.parseFloat(args[1]);
 
 		int edgeCnt = 0;
 		DirectedSparseGraph<String, Integer> graph = new DirectedSparseGraph<String, Integer>();
@@ -34,12 +36,12 @@ public class SequentialPageRank {
 			line.trim();
 			String[] arr = line.split("\\t");
 
-			//System.out.print(arr[0] + " ->");
+			// System.out.print(arr[0] + " ->");
 			for (int i = 1; i < arr.length; i++) {
 				graph.addEdge(new Integer(edgeCnt++), arr[0], arr[i]);
-				//System.out.print(" " + arr[i]);
+				// System.out.print(" " + arr[i]);
 			}
-			//System.out.print("\n");
+			// System.out.print("\n");
 		}
 
 		data.close();
@@ -50,11 +52,15 @@ public class SequentialPageRank {
 				.transform(graph);
 		int numComponents = components.size();
 		System.out.println("Number of components: " + numComponents);
+		System.out.println("Number of edges: " + graph.getEdgeCount());
+		System.out.println("Number of nodes: " + graph.getVertexCount());
+		System.out.println("Damping factor: " + alpha);
 
 		PageRank<String, Integer> ranker = new PageRank<String, Integer>(graph,
-				0.15);
+				alpha);
 		ranker.evaluate();
 
+		System.out.println("\nPageRank of nodes, in descending order:");
 		for (Ranking<?> s : (List<Ranking<?>>) ranker.getRankings()) {
 			String pmid = s.getRanked().toString();
 
