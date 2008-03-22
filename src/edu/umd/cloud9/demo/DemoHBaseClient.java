18dc68944271529a9f3b11d7cece88700c24bcc6
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-03-22 16:29:32 +0000

    added comments

--- src/edu/umd/cloud9/demo/DemoHBaseClient.java
+++ src/edu/umd/cloud9/demo/DemoHBaseClient.java
@@ -1,3 +1,19 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package edu.umd.cloud9.demo;
 
 import java.io.IOException;
@@ -10,6 +26,9 @@ import org.apache.hadoop.hbase.HStoreKey;
 import org.apache.hadoop.hbase.HTable;
 import org.apache.hadoop.io.Text;
 
+/**
+ * Demo that illustrates the HBase client API.
+ */
 public class DemoHBaseClient {
 
 	public static void main(String[] args) throws IOException {
@@ -37,13 +56,15 @@ public class DemoHBaseClient {
 			table.put(writeid, col, val.getBytes());
 			table.commit(writeid);
 
-			System.out.println("Writing row = " + rowid + ", col '" + col + "' = " + val);
+			System.out.println("Writing row = " + rowid + ", col '" + col
+					+ "' = " + val);
 		}
 
 		// illustration of querying
 		System.out.println("\nIllustration of querying...");
 
-		String s = Text.decode(table.get(new Text("1"), new Text("family1:col1")));
+		String s = Text.decode(table.get(new Text("1"),
+				new Text("family1:col1")));
 		System.out.println("row = 1, 'family1:col1' = " + s);
 
 		// illustration of scanning
@@ -62,7 +83,8 @@ public class DemoHBaseClient {
 		while (scanner.next(currentKey, values)) {
 			// decode the stored byte[] back into a String
 			String val = Text.decode(values.get(col));
-			System.out.println("row = " + currentKey + ", col '" + col + "' = " + val);
+			System.out.println("row = " + currentKey + ", col '" + col + "' = "
+					+ val);
 		}
 
 		// remember to close scanner when done
