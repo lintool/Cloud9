41838436dd517b0c4123abfe6fbb3164e9fc6e5d
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-06-21 03:04:55 +0000

    javadoc

--- src/edu/umd/cloud9/tuple/HashMapWritable.java
+++ src/edu/umd/cloud9/tuple/HashMapWritable.java
@@ -1,3 +1,19 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package edu.umd.cloud9.tuple;
 
 import java.io.ByteArrayInputStream;
@@ -16,6 +32,29 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Writable;
 
+/**
+ * <p>
+ * HashMap that is serializable in Hadoop framework. There are a number of key
+ * differences between this class and Hadoop's {@link MapWritable}:
+ * </p>
+ * 
+ * <ul>
+ * 
+ * <li><code>MapWritable</code> is more flexible in that it supports
+ * heterogeneous elements. In this class, all keys must be of the same type and
+ * all values must be of the same type. This assumption allows a simpler
+ * serialization protocol and thus is more efficient. Run <code>main</code> in
+ * this class for a simple efficiency test.</li>
+ * 
+ * <li>This class is generic, whereas <code>MapWritable</code> isn't.</li>
+ * 
+ * </ul>
+ * 
+ * @param <K>
+ *            type of the key
+ * @param <V>
+ *            type of the value
+ */
 public class HashMapWritable<K extends Writable, V extends Writable> extends HashMap<K, V>
 		implements Writable {
 
