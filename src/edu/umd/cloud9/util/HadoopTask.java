27349b6d53d9f16f94e24842a14a8b0dc1ecb9eb
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-07-15 16:01:02 +0000

    abstract base class for Hadoop tasks

---
+++ src/edu/umd/cloud9/util/HadoopTask.java
@@ -0,0 +1,45 @@
+package edu.umd.cloud9.util;
+
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+
+public abstract class HadoopTask implements Configurable {
+
+	protected Configuration mConf;
+
+	public HadoopTask(Configuration conf) {
+		mConf = conf;
+
+		if (getRequiredParameters() == null)
+			throw new RuntimeException("Error: getRequiredParameters() returns null!");
+	}
+
+	public void checkRequiredParametersPresent() {
+		if (getRequiredParameters() == null)
+			throw new RuntimeException("Error: getRequiredParameters() returns null!");
+
+		for (String param : getRequiredParameters()) {
+			if (mConf.get(param) == null) {
+				throw new RuntimeException("Error: required parameter '" + param + "' not defined");
+			}
+		}
+	}
+
+	public Configuration getConf() {
+		return mConf;
+	}
+
+	public void setConf(Configuration conf) {
+		mConf = conf;
+	}
+
+	public void run() throws Exception {
+		checkRequiredParametersPresent();
+
+		runTask();
+	}
+
+	protected abstract void runTask() throws Exception;
+
+	public abstract String[] getRequiredParameters();
+}
