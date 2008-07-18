3aac179ecfa532cafdc644533f4162e1062fd4e1
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-07-18 22:13:51 +0000

    changed scope of mConfig to private

--- src/edu/umd/cloud9/util/HadoopTask.java
+++ src/edu/umd/cloud9/util/HadoopTask.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.conf.Configuration;
 
 public abstract class HadoopTask implements Configurable {
 
-	protected Configuration mConf;
+	private Configuration mConf;
 
 	public HadoopTask(Configuration conf) {
 		mConf = conf;
