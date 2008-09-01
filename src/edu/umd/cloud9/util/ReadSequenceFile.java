0618a7308c4ac0ecace8714581123cd97e9f37fd
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-09-01 04:10:20 +0000

    added boilerplate, javadoc

--- src/edu/umd/cloud9/util/ReadSequenceFile.java
+++ src/edu/umd/cloud9/util/ReadSequenceFile.java
@@ -1,3 +1,19 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package edu.umd.cloud9.util;
 
 import java.io.IOException;
@@ -10,11 +26,23 @@ import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapred.JobConf;
 
+/**
+ * <p>
+ * Program that reads either a SequenceFile or a directory containing
+ * SequenceFiles. A maximum number of key-value pairs to read must be specified;
+ * in the of a directory, the value specifies the number of key-value pairs to
+ * read <i>per file</i>.
+ * </p>
+ * 
+ * <pre>
+ * args: [path] [max-num-of-records]
+ * </pre>
+ */
 public class ReadSequenceFile {
 
 	public static void main(String[] args) throws IOException {
 		if (args.length < 1) {
-			System.out.println("Usage: ReadSequenceFile [file] [max-num-of-records]");
+			System.out.println("args: [path] [max-num-of-records]");
 			System.exit(-1);
 		}
 
--- src/edu/umd/cloud9/util/SequenceFileUtils.java
+++ src/edu/umd/cloud9/util/SequenceFileUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package edu.umd.cloud9.util;
 
 import java.io.IOException;
@@ -15,13 +31,37 @@ import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapred.JobConf;
 
+/**
+ * Class containing a number of utility methods for manipulating SequenceFiles.
+ */
 public class SequenceFileUtils {
 
+	private SequenceFileUtils() {
+	}
+	
+	/**
+	 * Reads key-value pairs from a SequenceFile, up to a maximum number.
+	 * 
+	 * @param path
+	 *            path to file (as a String)
+	 * @param max
+	 *            maximum of key-value pairs to read
+	 * @return list of key-value pairs
+	 */
 	public static List<KeyValuePair<WritableComparable, Writable>> readFile(String path, int max) {
 
 		return readFile(new Path(path), max);
 	}
 
+	/**
+	 * Reads key-value pairs from a SequenceFile, up to a maximum number.
+	 * 
+	 * @param path
+	 *            path to file (as a Path)
+	 * @param max
+	 *            maximum of key-value pairs to read
+	 * @return list of key-value pairs
+	 */
 	public static List<KeyValuePair<WritableComparable, Writable>> readFile(Path path, int max) {
 
 		List<KeyValuePair<WritableComparable, Writable>> list = new ArrayList<KeyValuePair<WritableComparable, Writable>>();
@@ -54,12 +94,32 @@ public class SequenceFileUtils {
 		return list;
 	}
 
+	/**
+	 * Reads key-value pairs from a directory containing SequenceFiles. A
+	 * maximum number of key-value pairs is read from each SequenceFile.
+	 * 
+	 * @param path
+	 *            path to directory (as a String)
+	 * @param max
+	 *            maximum of key-value pairs to read per file
+	 * @return list of key-value pairs
+	 */
 	public static List<KeyValuePair<WritableComparable, Writable>> readDirectory(String path,
 			int max) {
 
 		return readDirectory(new Path(path), max);
 	}
 
+	/**
+	 * Reads key-value pairs from a directory containing SequenceFiles. A
+	 * maximum number of key-value pairs is read from each SequenceFile.
+	 * 
+	 * @param path
+	 *            path to directory (as a Path)
+	 * @param max
+	 *            maximum of key-value pairs to read per file
+	 * @return list of key-value pairs
+	 */
 	public static List<KeyValuePair<WritableComparable, Writable>> readDirectory(Path path, int max) {
 
 		List<KeyValuePair<WritableComparable, Writable>> list = new ArrayList<KeyValuePair<WritableComparable, Writable>>();
