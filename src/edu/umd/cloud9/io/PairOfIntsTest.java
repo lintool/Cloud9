2b28829e1f1f5bdf790441145a9e6026dd9e9c74
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-10-16 20:19:50 +0000

    added test case for comparator optimization

--- src/edu/umd/cloud9/io/PairOfIntsTest.java
+++ src/edu/umd/cloud9/io/PairOfIntsTest.java
@@ -17,8 +17,8 @@
 package edu.umd.cloud9.io;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -28,6 +28,7 @@ import java.io.IOException;
 
 import junit.framework.JUnit4TestAdapter;
 
+import org.apache.hadoop.io.WritableComparator;
 import org.junit.Test;
 
 public class PairOfIntsTest {
@@ -55,15 +56,14 @@ public class PairOfIntsTest {
 
 		PairOfInts pair = new PairOfInts();
 
-		pair.readFields(new DataInputStream(new ByteArrayInputStream(bytesOut
-				.toByteArray())));
+		pair.readFields(new DataInputStream(new ByteArrayInputStream(bytesOut.toByteArray())));
 
 		assertEquals(pair.getLeftElement(), 1);
 		assertEquals(pair.getRightElement(), 2);
 	}
 
 	@Test
-	public void testComparison() throws IOException {
+	public void testComparison1() throws IOException {
 		PairOfInts pair1 = new PairOfInts();
 		pair1.set(1, 2);
 
@@ -78,7 +78,7 @@ public class PairOfIntsTest {
 
 		PairOfInts pair5 = new PairOfInts();
 		pair5.set(9, 0);
-		
+
 		assertTrue(pair1.equals(pair2));
 		assertFalse(pair1.equals(pair3));
 
@@ -87,7 +87,62 @@ public class PairOfIntsTest {
 		assertTrue(pair1.compareTo(pair4) > 0);
 		assertTrue(pair1.compareTo(pair5) < 0);
 		assertTrue(pair3.compareTo(pair4) > 0);
-		assertTrue(pair4.compareTo(pair5) < 0);		
+		assertTrue(pair4.compareTo(pair5) < 0);
+	}
+
+	@Test
+	public void testComparison2() throws IOException {
+		WritableComparator comparator = new PairOfInts.Comparator();
+
+		PairOfInts pair1 = new PairOfInts();
+		pair1.set(1, 2);
+
+		ByteArrayOutputStream bytesOut1 = new ByteArrayOutputStream();
+		DataOutputStream dataOut1 = new DataOutputStream(bytesOut1);
+		pair1.write(dataOut1);
+		byte[] bytes1 = bytesOut1.toByteArray();
+
+		PairOfInts pair2 = new PairOfInts();
+		pair2.set(1, 2);
+
+		ByteArrayOutputStream bytesOut2 = new ByteArrayOutputStream();
+		DataOutputStream dataOut2 = new DataOutputStream(bytesOut2);
+		pair2.write(dataOut2);
+		byte[] bytes2 = bytesOut2.toByteArray();
+
+		PairOfInts pair3 = new PairOfInts();
+		pair3.set(1, 1);
+
+		ByteArrayOutputStream bytesOut3 = new ByteArrayOutputStream();
+		DataOutputStream dataOut3 = new DataOutputStream(bytesOut3);
+		pair3.write(dataOut3);
+		byte[] bytes3 = bytesOut3.toByteArray();
+
+		PairOfInts pair4 = new PairOfInts();
+		pair4.set(0, 9);
+
+		ByteArrayOutputStream bytesOut4 = new ByteArrayOutputStream();
+		DataOutputStream dataOut4 = new DataOutputStream(bytesOut4);
+		pair4.write(dataOut4);
+		byte[] bytes4 = bytesOut4.toByteArray();
+
+		PairOfInts pair5 = new PairOfInts();
+		pair5.set(9, 0);
+
+		ByteArrayOutputStream bytesOut5 = new ByteArrayOutputStream();
+		DataOutputStream dataOut5 = new DataOutputStream(bytesOut5);
+		pair5.write(dataOut5);
+		byte[] bytes5 = bytesOut5.toByteArray();
+
+		assertTrue(pair1.equals(pair2));
+		assertFalse(pair1.equals(pair3));
+
+		assertTrue(comparator.compare(bytes1, 0, bytes1.length, bytes2, 0, bytes2.length) == 0);
+		assertTrue(comparator.compare(bytes1, 0, bytes1.length, bytes3, 0, bytes3.length) > 0);
+		assertTrue(comparator.compare(bytes1, 0, bytes1.length, bytes4, 0, bytes4.length) > 0);
+		assertTrue(comparator.compare(bytes1, 0, bytes1.length, bytes5, 0, bytes5.length) < 0);
+		assertTrue(comparator.compare(bytes3, 0, bytes3.length, bytes4, 0, bytes4.length) > 0);
+		assertTrue(comparator.compare(bytes4, 0, bytes4.length, bytes5, 0, bytes5.length) < 0);
 	}
 
 	public static junit.framework.Test suite() {
