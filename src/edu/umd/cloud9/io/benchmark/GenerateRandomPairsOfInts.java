829d1cfb17f201fe2733a16e0151b4c6b0c2d4f9
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-10-16 14:30:40 +0000

    1m PairOfInts (each populated with two randomly-generated ints between 0 and 1000)

---
+++ src/edu/umd/cloud9/io/benchmark/GenerateRandomPairsOfInts.java
@@ -0,0 +1,35 @@
+package edu.umd.cloud9.io.benchmark;
+
+import java.util.Random;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+
+import edu.umd.cloud9.io.PairOfInts;
+
+public class GenerateRandomPairsOfInts {
+
+	public static void main(String[] args) throws Exception {
+		Random r = new Random();
+
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(conf);
+		SequenceFile.Writer writer = SequenceFile.createWriter(fs, conf, new Path(
+				"random-pairs.seq"), PairOfInts.class, IntWritable.class);
+
+		IntWritable n = new IntWritable();
+		PairOfInts pair = new PairOfInts();
+
+		for (int i = 0; i < 1000000; i++) {
+			n.set(i);
+			pair.set(r.nextInt(1000), r.nextInt(1000));
+			writer.append(pair, n);
+		}
+
+		writer.close();
+
+	}
+}
