234c77afad72f7b7d7780cee5e8b35a9b3453eb1
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-10-31 21:58:48 +0000

    simple utils to check for memory usage, from Java Platform Performance: Strategies and Tactics http://java.sun.com/docs/books/performance/

---
+++ src/edu/umd/cloud9/debug/MemoryUsageUtils.java
@@ -0,0 +1,29 @@
+package edu.umd.cloud9.debug;
+
+public class MemoryUsageUtils {
+
+	public static long getUsedMemory() {
+		gc();
+		long totalMemory = Runtime.getRuntime().totalMemory();
+		gc();
+		long freeMemory = Runtime.getRuntime().freeMemory();
+		long usedMemory = totalMemory - freeMemory;
+		return usedMemory;
+	}
+
+	public static void gc() {
+		try {
+			System.gc();
+			Thread.sleep(100);
+			System.runFinalization();
+			Thread.sleep(100);
+			System.gc();
+			Thread.sleep(100);
+			System.runFinalization();
+			Thread.sleep(100);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
