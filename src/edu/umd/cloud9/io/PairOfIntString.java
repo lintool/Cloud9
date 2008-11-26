90958944ee69544ffe5f9b2121e80ee7e9cc7bdc
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-11-26 18:59:13 +0000

    pair consisting of an int and a string

---
+++ src/edu/umd/cloud9/io/PairOfIntString.java
@@ -0,0 +1,170 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package edu.umd.cloud9.io;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.WritableComparable;
+
+/**
+ * <p>
+ * WritableComparable representing a pair of Strings. The elements in the pair
+ * are referred to as the left and right elements. The natural sort order is:
+ * first by the left element, and then by the right element.
+ * </p>
+ */
+public class PairOfIntString implements WritableComparable {
+
+	private int leftElement; 
+	private String rightElement;
+
+	/**
+	 * Creates a pair.
+	 */
+	public PairOfIntString() {
+	}
+
+	/**
+	 * Creates a pair.
+	 * 
+	 * @param left
+	 *            the left element
+	 * @param right
+	 *            the right element
+	 */
+	public PairOfIntString(int left, String right) {
+		set(left, right);
+	}
+
+	/**
+	 * Deserializes the pair.
+	 * 
+	 * @param in
+	 *            source for raw byte representation
+	 */
+	public void readFields(DataInput in) throws IOException {
+		leftElement = in.readInt();
+		rightElement = in.readUTF();
+	}
+
+	/**
+	 * Serializes this pair.
+	 * 
+	 * @param out
+	 *            where to write the raw byte representation
+	 */
+	public void write(DataOutput out) throws IOException {
+		out.writeInt(leftElement);
+		out.writeUTF(rightElement);
+	}
+
+	/**
+	 * Returns the left element.
+	 * 
+	 * @return the left element
+	 */
+	public int getLeftElement() {
+		return leftElement;
+	}
+
+	/**
+	 * Returns the right element.
+	 * 
+	 * @return the right element
+	 */
+	public String getRightElement() {
+		return rightElement;
+	}
+
+	/**
+	 * Sets the right and left elements of this pair.
+	 * 
+	 * @param left
+	 *            the left element
+	 * @param right
+	 *            the right element
+	 */
+	public void set(int left, String right) {
+		leftElement = left;
+		rightElement = right;
+	}
+
+	/**
+	 * Checks two pairs for equality.
+	 * 
+	 * @param obj
+	 *            object for comparison
+	 * @return <code>true</code> if <code>obj</code> is equal to this
+	 *         object, <code>false</code> otherwise
+	 */
+	public boolean equals(Object obj) {
+		PairOfIntString pair = (PairOfIntString) obj;
+		return leftElement == pair.getLeftElement()
+				&& rightElement.equals(pair.getRightElement());
+	}
+
+	/**
+	 * Defines a natural sort order for pairs. Pairs are sorted first by the
+	 * left element, and then by the right element.
+	 * 
+	 * @return a value less than zero, a value greater than zero, or zero if
+	 *         this pair should be sorted before, sorted after, or is equal to
+	 *         <code>obj</code>.
+	 */
+	public int compareTo(Object obj) {
+		PairOfIntString pair = (PairOfIntString) obj;
+
+		int pl = pair.getLeftElement();
+		String pr = pair.getRightElement();
+
+		if (leftElement == pl) {
+			return rightElement.compareTo(pr);
+		}
+
+		return leftElement < pl ? -1 : 1;
+	}
+
+	/**
+	 * Returns a hash code value for the pair.
+	 * 
+	 * @return hash code for the pair
+	 */
+	public int hashCode() {
+		return leftElement + rightElement.hashCode();
+	}
+
+	/**
+	 * Generates human-readable String representation of this pair.
+	 * 
+	 * @return human-readable String representation of this pair
+	 */
+	public String toString() {
+		return "(" + leftElement + ", " + rightElement + ")";
+	}
+
+	/**
+	 * Clones this object.
+	 * 
+	 * @return clone of this object
+	 */
+	public PairOfIntString clone() {
+		return new PairOfIntString(this.leftElement, this.rightElement);
+	}
+
+}
