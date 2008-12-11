50abc55d803cf01f74da2bf5d75095cbaae4762a
abahety@cs.umd.edu <abahety@cs.umd.edu@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-12-11 19:55:04 +0000

    Commented the file

--- src/edu/umd/cloud9/memcache/WordLogProb/GetLogProbFromHDFS.java
+++ src/edu/umd/cloud9/memcache/WordLogProb/GetLogProbFromHDFS.java
@@ -21,20 +21,36 @@ import org.apache.hadoop.mapred.lib.IdentityReducer;
 
 import edu.umd.cloud9.io.Tuple;
 
+/**
+ * This class is used to get log probabilities from HDFS on map reduce framework.
+ * It opens a map file on HDFS in the map cycle to get the log probability of the words in the line passed in as a key to map cycle
+ * @author Shravya
+ *
+ */
 public class GetLogProbFromHDFS {
+	
+	/*
+	 * This is used to add up total time for access to HDFS in map cycle	 
+	 */
 	static enum MyCounters {
 		TIME;
 	};
 
+	/*
+	 * Mapper class : Tokenizes the string and just accesses log probabilities from file on HDFS
+	 */
 	public static class MyMapper extends MapReduceBase implements
 			Mapper<LongWritable, Tuple, LongWritable, FloatWritable> {
 
-		Long keyTemp = new Long(0);
+	    Long keyTemp = new Long(0);
 		Object obj;
 		String inputPath;
 		FileSystem fs;
 		MapFile.Reader reader;
 
+		/*
+		 * This function opens a MAP-fomat file on HDFS to access log probabilities of the word 
+		 */
 		public void configure(JobConf conf) {
 			try {
 				inputPath = conf.get("mapfileInputPath");
@@ -45,38 +61,46 @@ public class GetLogProbFromHDFS {
 			}
 		}
 
-		public void map(LongWritable key, Tuple value,
-				OutputCollector<LongWritable, FloatWritable> output, Reporter reporter)
+		/*
+		 * Tokenizes the string. For each word, access the log probability of it from file on HDFS
+		 */
+		public void map(LongWritable key, Tuple value, OutputCollector<LongWritable, FloatWritable> output, Reporter reporter)
 				throws IOException {
-			FloatWritable fw = new FloatWritable();
+			FloatWritable logProb = new FloatWritable();
 			FloatWritable finalValue = new FloatWritable();
 			String line = (String) value.get(0);
 			StringTokenizer itr = new StringTokenizer(line);
 
 			float sum = 0;
 			Text tempKey = new Text();
-
+			// tokenize word in the line
 			while (itr.hasMoreTokens()) {
 				String temp = itr.nextToken();
 				tempKey.set(temp);
-
+				
+				// start timer
 				long startTime = System.currentTimeMillis();
+				// access MAP file on HDFS
 				reader.seek(tempKey);
-				reader.get(tempKey, fw);
+				reader.get(tempKey, logProb);
+				// end timer
 				long endTime = System.currentTimeMillis();
 				long diff = (endTime - startTime);
+				// incrementing the count to access each single record
 				reporter.incrCounter(MyCounters.TIME, diff);
-				if (fw == null)
+				if (logProb == null)
 					throw new RuntimeException("Error getting from memcache");
 
-				sum += (float) fw.get();
+				sum += (float) logProb.get();
 			}
 
 			finalValue.set(sum);
 			output.collect(key, finalValue);
 		}
 	}
-
+	/*
+	 * Takes in three arguments
+	 */
 	public static void main(String[] args) throws IOException {
 
 		if (args.length != 3) {
@@ -92,7 +116,7 @@ public class GetLogProbFromHDFS {
 		String extraPath = "/shared/extraInfo";
 
 		JobConf conf = new JobConf(GetLogProbFromHDFS.class);
-		conf.setJobName("GetFromFS");
+		conf.setJobName("GetLogProbFromHDFS");
 
 		conf.setNumMapTasks(mapTasks);
 		conf.setNumReduceTasks(reduceTasks);
@@ -103,18 +127,14 @@ public class GetLogProbFromHDFS {
 		conf.setMapOutputValueClass(FloatWritable.class);
 		conf.setMapperClass(MyMapper.class);
 		conf.setReducerClass(IdentityReducer.class);
+		// setting path to MAP file containing log probability so that it can be accessed  through mapper
 		conf.set("mapfileInputPath", mapPath);
 
 		Path outputDir = new Path(extraPath);
 		FileSystem.get(conf).delete(outputDir, true);
 		FileOutputFormat.setOutputPath(conf, outputDir);
 
-		long startTime = System.currentTimeMillis();
 		JobClient.runJob(conf);
-		long endTime = System.currentTimeMillis();
-		float diff = (float) ((endTime - startTime));
-		System.out.println("\n Starttime " + startTime + " end time = " + endTime);
-		System.out.println("\n Total time taken is : " + diff);
 	}
 
 }
