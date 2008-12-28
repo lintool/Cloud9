0fbf020bac0ced070a6ec0ef59665b3f4d02688e
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-12-28 17:40:07 +0000

    these are going to be improved versions of VectorInt and VectorFloat

---
+++ src/edu/umd/cloud9/io/KeyToIntMap.java
@@ -0,0 +1,223 @@
+package edu.umd.cloud9.io;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+
+/**
+ * <p>
+ * Writable representing a feature vector of integer values. This generic class,
+ * based on the Java {@link HashMap}, supports the use of any class for the
+ * features (i.e., component of the vector), but all values are integers.
+ * </p>
+ * 
+ * @param <F>
+ *            type of feature
+ */
+public class KeyToIntMap<F extends WritableComparable> extends HashMap<F, Integer> implements
+		Writable {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Creates a KeyToIntMap object.
+	 */
+	public KeyToIntMap() {
+		super();
+	}
+
+	/**
+	 * Deserializes the vector.
+	 * 
+	 * @param in
+	 *            source for raw byte representation
+	 */
+	@SuppressWarnings("unchecked")
+	public void readFields(DataInput in) throws IOException {
+
+		this.clear();
+
+		int numEntries = in.readInt();
+		if (numEntries == 0)
+			return;
+
+		String keyClassName = in.readUTF();
+
+		F objK;
+
+		try {
+			Class keyClass = Class.forName(keyClassName);
+			for (int i = 0; i < numEntries; i++) {
+				objK = (F) keyClass.newInstance();
+				objK.readFields(in);
+				Integer s = in.readInt();
+				put(objK, s);
+			}
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Serializes the vector.
+	 * 
+	 * @param out
+	 *            where to write the raw byte representation
+	 */
+	public void write(DataOutput out) throws IOException {
+		// Write out the number of entries in the map
+		out.writeInt(size());
+		if (size() == 0)
+			return;
+
+		// Write out the class names for keys and values
+		// assuming that data is homogeneous (i.e., all entries have same types)
+		Set<Map.Entry<F, Integer>> entries = entrySet();
+		Map.Entry<F, Integer> first = entries.iterator().next();
+		F objK = first.getKey();
+		out.writeUTF(objK.getClass().getCanonicalName());
+
+		// Then write out each key/value pair
+		for (Map.Entry<F, Integer> e : entrySet()) {
+			e.getKey().write(out);
+			out.writeInt(e.getValue());
+		}
+	}
+
+	/**
+	 * Sets the value of a feature.
+	 * 
+	 * @param f
+	 *            the feature
+	 * @param v
+	 *            the value
+	 */
+	public void set(F f, int v) {
+		super.put(f, v);
+	}
+
+	/**
+	 * Returns the value of a feature.
+	 * 
+	 * @param f
+	 *            the feature
+	 * @return the value of the feature
+	 */
+	public int get(F f) {
+		return super.get(f);
+	}
+
+	/**
+	 * Adds another vector to this vector, based on feature-wise addition.
+	 * 
+	 * @param v
+	 *            vector to add
+	 */
+	public void plus(KeyToIntMap<F> v) {
+		for (Map.Entry<F, Integer> e : v.entrySet()) {
+			F key = e.getKey();
+
+			if (this.containsKey(key)) {
+				this.put(key, this.get(key) + e.getValue());
+			} else {
+				this.put(key, e.getValue());
+			}
+		}
+	}
+
+	/**
+	 * Computes the dot product between this vector and another vector.
+	 * 
+	 * @param v
+	 *            the other vector
+	 */
+	public int dot(KeyToIntMap<F> v) {
+		int s = 0;
+
+		for (Map.Entry<F, Integer> e : v.entrySet()) {
+			F key = e.getKey();
+
+			if (this.containsKey(key)) {
+				s += this.get(key) * e.getValue();
+			}
+		}
+
+		return s;
+	}
+
+	/**
+	 * Returns feature-value entries sorted by descending value. Ties broken by
+	 * the natural sort order of the feature.
+	 * 
+	 * @return feature-value entries sorted by descending value
+	 */
+	public SortedSet<Map.Entry<F, Integer>> getSortedEntries() {
+		SortedSet<Map.Entry<F, Integer>> entries = new TreeSet<Map.Entry<F, Integer>>(
+				new Comparator<Map.Entry<F, Integer>>() {
+					@SuppressWarnings("unchecked")
+					public int compare(Map.Entry<F, Integer> e1, Map.Entry<F, Integer> e2) {
+						if (e1.getValue() > e2.getValue()) {
+							return -1;
+						} else if (e1.getValue() < e2.getValue()) {
+							return 1;
+						}
+						return e1.getKey().compareTo(e2.getKey());
+					}
+				});
+
+		for (Map.Entry<F, Integer> entry : this.entrySet()) {
+			entries.add(entry);
+		}
+
+		return Collections.unmodifiableSortedSet(entries);
+	}
+
+	/**
+	 * Returns top <i>n</i> feature-value entries sorted by descending value.
+	 * Ties broken by the natural sort order of the feature.
+	 * 
+	 * @param n
+	 *            number of entries to return
+	 * @return top <i>n</i> feature-value entries sorted by descending value
+	 */
+	public SortedSet<Map.Entry<F, Integer>> getSortedEntries(int n) {
+		SortedSet<Map.Entry<F, Integer>> entries = new TreeSet<Map.Entry<F, Integer>>(
+				new Comparator<Map.Entry<F, Integer>>() {
+					@SuppressWarnings("unchecked")
+					public int compare(Map.Entry<F, Integer> e1, Map.Entry<F, Integer> e2) {
+						if (e1.getValue() > e2.getValue()) {
+							return -1;
+						} else if (e1.getValue() < e2.getValue()) {
+							return 1;
+						}
+						return e1.getKey().compareTo(e2.getKey());
+					}
+				});
+
+		int cnt = 0;
+		for (Map.Entry<F, Integer> entry : getSortedEntries()) {
+			entries.add(entry);
+			cnt++;
+			if (cnt >= n)
+				break;
+		}
+
+		return Collections.unmodifiableSortedSet(entries);
+	}
+}
---
+++ src/edu/umd/cloud9/io/KeyToIntMapTest.java
@@ -0,0 +1,233 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package edu.umd.cloud9.io;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+import junit.framework.JUnit4TestAdapter;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.junit.Test;
+
+public class KeyToIntMapTest {
+
+	@Test
+	public void testBasic() throws IOException {
+		KeyToIntMap<Text> v = new KeyToIntMap<Text>();
+
+		v.set(new Text("hi"), 5);
+		v.set(new Text("there"), 22);
+
+		Text key;
+		int value;
+
+		assertEquals(v.size(), 2);
+
+		key = new Text("hi");
+		value = v.get(key);
+		assertEquals(value, 5);
+
+		value = v.remove(key);
+		assertEquals(v.size(), 1);
+
+		key = new Text("there");
+		value = v.get(key);
+		assertEquals(value, 22);
+	}
+
+	@Test
+	public void testSerialize1() throws IOException {
+		KeyToIntMap<Text> v1 = new KeyToIntMap<Text>();
+
+		v1.set(new Text("hi"), 5);
+		v1.set(new Text("there"), 22);
+
+		ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+		DataOutputStream dataOut = new DataOutputStream(bytesOut);
+
+		v1.write(dataOut);
+
+		KeyToIntMap<Text> v2 = new KeyToIntMap<Text>();
+
+		v2.readFields(new DataInputStream(new ByteArrayInputStream(bytesOut.toByteArray())));
+
+		Text key;
+		int value;
+
+		assertEquals(v2.size(), 2);
+
+		key = new Text("hi");
+		value = v2.get(key);
+		assertEquals(value, 5);
+
+		value = v2.remove(key);
+		assertEquals(v2.size(), 1);
+
+		key = new Text("there");
+		value = v2.get(key);
+		assertEquals(value, 22);
+	}
+
+	@Test(expected = IOException.class)
+	public void testTypeSafety() throws IOException {
+		KeyToIntMap<WritableComparable> v1 = new KeyToIntMap<WritableComparable>();
+
+		v1.set(new Text("hi"), 4);
+		v1.set(new IntWritable(0), 76);
+
+		ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+		DataOutputStream dataOut = new DataOutputStream(bytesOut);
+
+		v1.write(dataOut);
+
+		KeyToIntMap<WritableComparable> v2 = new KeyToIntMap<WritableComparable>();
+
+		v2.readFields(new DataInputStream(new ByteArrayInputStream(bytesOut.toByteArray())));
+
+	}
+
+	@Test
+	public void testSerializeEmpty() throws IOException {
+		KeyToIntMap<WritableComparable> v1 = new KeyToIntMap<WritableComparable>();
+
+		assertTrue(v1.size() == 0);
+
+		ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+		DataOutputStream dataOut = new DataOutputStream(bytesOut);
+
+		v1.write(dataOut);
+
+		KeyToIntMap<WritableComparable> v2 = new KeyToIntMap<WritableComparable>();
+		v2.readFields(new DataInputStream(new ByteArrayInputStream(bytesOut.toByteArray())));
+		assertTrue(v2.size() == 0);
+	}
+
+	@Test
+	public void testPlus() throws IOException {
+		KeyToIntMap<Text> v1 = new KeyToIntMap<Text>();
+
+		v1.set(new Text("hi"), 5);
+		v1.set(new Text("there"), 22);
+
+		KeyToIntMap<Text> v2 = new KeyToIntMap<Text>();
+
+		v2.set(new Text("hi"), 4);
+		v2.set(new Text("test"), 5);
+
+		v1.plus(v2);
+
+		assertEquals(v1.size(), 3);
+		assertTrue(v1.get(new Text("hi")) == 9);
+		assertTrue(v1.get(new Text("there")) == 22);
+		assertTrue(v1.get(new Text("test")) == 5);
+	}
+
+	@Test
+	public void testDot() throws IOException {
+		KeyToIntMap<Text> v1 = new KeyToIntMap<Text>();
+
+		v1.set(new Text("hi"), 5);
+		v1.set(new Text("there"), 2);
+		v1.set(new Text("empty"), 3);
+
+		KeyToIntMap<Text> v2 = new KeyToIntMap<Text>();
+
+		v2.set(new Text("hi"), 4);
+		v2.set(new Text("there"), 4);
+		v2.set(new Text("test"), 5);
+
+		int s = v1.dot(v2);
+
+		assertEquals(s, 28);
+	}
+
+	@Test
+	public void testSortedEntries1() {
+
+		KeyToIntMap<Text> v = new KeyToIntMap<Text>();
+
+		v.set(new Text("a"), 5);
+		v.set(new Text("b"), 2);
+		v.set(new Text("c"), 3);
+		v.set(new Text("d"), 3);
+		v.set(new Text("e"), 1);
+
+		Iterator<Map.Entry<Text, Integer>> iter = v.getSortedEntries().iterator();
+
+		Map.Entry<Text, Integer> m = iter.next();
+		assertEquals(new Text("a"), m.getKey());
+		assertEquals(5, (int) m.getValue());
+
+		m = iter.next();
+		assertEquals(new Text("c"), m.getKey());
+		assertEquals(3, (int) m.getValue());
+
+		m = iter.next();
+		assertEquals(new Text("d"), m.getKey());
+		assertEquals(3, (int) m.getValue());
+
+		m = iter.next();
+		assertEquals(new Text("b"), m.getKey());
+		assertEquals(2, (int) m.getValue());
+
+		m = iter.next();
+		assertEquals(new Text("e"), m.getKey());
+		assertEquals(1, (int) m.getValue());
+
+		assertEquals(false, iter.hasNext());		
+	}
+
+	@Test
+	public void testSortedEntries2() {
+
+		KeyToIntMap<Text> v = new KeyToIntMap<Text>();
+
+		v.set(new Text("a"), 5);
+		v.set(new Text("b"), 2);
+		v.set(new Text("c"), 3);
+		v.set(new Text("d"), 3);
+		v.set(new Text("e"), 1);
+
+		Iterator<Map.Entry<Text, Integer>> iter = v.getSortedEntries(2).iterator();
+
+		Map.Entry<Text, Integer> m = iter.next();
+		assertEquals(new Text("a"), m.getKey());
+		assertEquals(5, (int) m.getValue());
+
+		m = iter.next();
+		assertEquals(new Text("c"), m.getKey());
+		assertEquals(3, (int) m.getValue());
+
+		assertEquals(false, iter.hasNext());		
+	}
+	
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(KeyToIntMapTest.class);
+	}
+
+}
