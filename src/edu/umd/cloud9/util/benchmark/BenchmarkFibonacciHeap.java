4b96c4959aed3161f3a1fdcfcd21efe370c1f939
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2008-12-29 03:20:14 +0000

    benchmarks on Fibonacci heap implementations

---
+++ src/edu/umd/cloud9/util/benchmark/BenchmarkFibonacciHeap.java
@@ -0,0 +1,34 @@
+package edu.umd.cloud9.util.benchmark;
+
+import java.util.Random;
+
+import edu.umd.cloud9.util.FibonacciHeap;
+
+public class BenchmarkFibonacciHeap {
+	public static void main(String[] args) {
+		FibonacciHeap<Integer> heap = new FibonacciHeap<Integer>();
+		Random r = new Random();
+
+		long startTime;
+		long endTime;
+
+		startTime = System.currentTimeMillis();
+		for (int i = 0; i < 1000000; i++) {
+			int n = r.nextInt(1000);
+			float f = r.nextFloat();
+
+			heap.insert(n, f);
+		}
+
+		endTime = System.currentTimeMillis();
+		System.out.println("inserts completed in " + (endTime - startTime) + " ms");
+
+		long sum = 0;
+		startTime = System.currentTimeMillis();
+		for (int i = 0; i < 500000; i++) {
+			sum += heap.removeMin().getDatum();
+		}
+		endTime = System.currentTimeMillis();
+		System.out.println("mins completed in " + (endTime - startTime) + " ms");
+	}
+}
