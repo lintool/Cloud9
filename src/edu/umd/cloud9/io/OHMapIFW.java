1e5fea0e1dde5cca1131bd9b247e46560d0051af
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-01-22 17:21:22 +0000

    extends OHMapIF, make it a writable

---
+++ src/edu/umd/cloud9/io/OHMapIFW.java
@@ -0,0 +1,81 @@
+package edu.umd.cloud9.io;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+
+import edu.umd.cloud9.util.MapIF;
+import edu.umd.cloud9.util.OHMapIF;
+
+/**
+ * <p>
+ * Writable representing a map where the values are floats.
+ * </p>
+ * 
+ * @param <K>
+ *            type of key
+ */
+public class OHMapIFW extends OHMapIF implements Writable {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Creates a MapKeyToFloatWritable object.
+	 */
+	public OHMapIFW() {
+		super();
+	}
+
+	/**
+	 * Deserializes the map.
+	 * 
+	 * @param in
+	 *            source for raw byte representation
+	 */
+	@SuppressWarnings("unchecked")
+	public void readFields(DataInput in) throws IOException {
+
+		this.clear();
+
+		int numEntries = in.readInt();
+		if (numEntries == 0)
+			return;
+
+		for (int i = 0; i < numEntries; i++) {
+			//int k = WritableUtils.readVInt(in);
+			int k = in.readInt();
+			float s = in.readFloat();
+			put(k, s);
+		}
+
+	}
+
+	/**
+	 * Serializes the map.
+	 * 
+	 * @param out
+	 *            where to write the raw byte representation
+	 */
+	public void write(DataOutput out) throws IOException {
+		// Write out the number of entries in the map
+		out.writeInt(size());
+		if (size() == 0)
+			return;
+
+		for (MapIF.Entry e : entrySet()) {
+			//WritableUtils.writeVInt(out, e.getKey());
+			out.writeInt(e.getKey());
+			out.writeFloat(e.getValue());
+		}
+	}
+
+	/*
+	 * public static <T extends WritableComparable> OHMapIFW<T>
+	 * create(DataInput in) throws IOException { OHMapIFW<T> m = new OHMapIFW<T>();
+	 * m.readFields(in);
+	 * 
+	 * return m; }
+	 */
+}
