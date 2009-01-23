62a50f7b6c222fb7885cefef45844ecdd628f3f0
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-01-23 20:36:08 +0000

    added option to decode map in a lazy manner

--- src/edu/umd/cloud9/io/OHMapIFW.java
+++ src/edu/umd/cloud9/io/OHMapIFW.java
@@ -19,6 +19,8 @@ import edu.umd.cloud9.util.OHMapIF;
  */
 public class OHMapIFW extends OHMapIF implements Writable {
 
+	private static boolean sLazyDecode = false;
+
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -39,17 +41,40 @@ public class OHMapIFW extends OHMapIF implements Writable {
 
 		this.clear();
 
-		int numEntries = in.readInt();
+		numEntries = in.readInt();
 		if (numEntries == 0)
 			return;
 
-		for (int i = 0; i < numEntries; i++) {
-			// int k = WritableUtils.readVInt(in);
-			int k = in.readInt();
-			float s = in.readFloat();
-			put(k, s);
+		if (sLazyDecode) {
+			// lazy initialization; read into arrays
+			keys = new int[numEntries];
+			values = new float[numEntries];
+
+			for (int i = 0; i < numEntries; i++) {
+				keys[i] = in.readInt();
+				values[i] = in.readFloat();
+			}
+		} else {
+			// normal initialization; populate the map
+			for (int i = 0; i < numEntries; i++) {
+				put(in.readInt(), in.readFloat());
+			}
 		}
 
+		// decode();
+	}
+
+	int numEntries = 0;
+	int[] keys = null;
+	float[] values = null;
+
+	public void decode() throws IOException {
+		if (keys == null)
+			return;
+
+		for (int i = 0; i < keys.length; i++) {
+			put(keys[i], values[i]);
+		}
 	}
 
 	/**
@@ -78,4 +103,36 @@ public class OHMapIFW extends OHMapIF implements Writable {
 		return m;
 	}
 
+	public static void setLazyDecodeFlag(boolean b) {
+		sLazyDecode = b;
+	}
+
+	public static boolean getLazyDecodeFlag() {
+		return sLazyDecode;
+	}
+
+	public int[] getKeys() {
+		return keys;
+	}
+
+	public float[] getValues() {
+		return values;
+	}
+
+	public void plus(OHMapIFW m) {
+		if (!sLazyDecode) {
+			super.plus(m);
+		} else {
+			int[] k = m.getKeys();
+			float[] v = m.getValues();
+
+			for (int i = 0; i < k.length; i++) {
+				if (this.containsKey(k[i])) {
+					this.put(k[i], this.get(k[i]) + v[i]);
+				} else {
+					this.put(k[i], v[i]);
+				}
+			}
+		}
+	}
 }
