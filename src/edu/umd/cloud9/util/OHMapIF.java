5565050fe01b65db7f6befb9572eef8e6cd4d3f2
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-01-27 17:02:17 +0000

    Arrays.copyOfRange does not exist in Java 1.5, so I had to rewrite with entry-by-entry copying (probably less efficient)

--- src/edu/umd/cloud9/util/OHMapIF.java
+++ src/edu/umd/cloud9/util/OHMapIF.java
@@ -145,7 +145,16 @@ public class OHMapIF extends HMapIF {
 		if (entries.length < n)
 			return entries;
 
-		return Arrays.copyOfRange(entries, 0, n);
+		// return Arrays.copyOfRange(entries, 0, n);
+
+		// copyOfRange isn't available until Java 1.6, so it doesn't run on the
+		// Google/IBM cluster.
+		Entry[] r = new Entry[n];
+		for (int i = 0; i < n; i++) {
+			r[i] = entries[i];
+		}
+
+		return r;
 	}
 
 }
