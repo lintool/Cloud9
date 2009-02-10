d9063030554ca9be2e4abd682c9790bebb606963
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-02-10 23:04:04 +0000

    object for reading Wikipedia pages from raw bz2 archives, moved over from notebook.wikipedia repo

---
+++ src/edu/umd/cloud9/data/wikipedia/WikipediaPagesBz2InputStream.java
@@ -0,0 +1,54 @@
+package edu.umd.cloud9.data.wikipedia;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.tools.bzip2.CBZip2InputStream;
+
+public class WikipediaPagesBz2InputStream {
+	private static int DEFAULT_STRINGBUFFER_CAPACITY = 1024;
+
+	BufferedReader br;
+	FileInputStream fis;
+
+	public WikipediaPagesBz2InputStream(String file) throws IOException {
+		br = null;
+		fis = new FileInputStream(file);
+		byte[] ignoreBytes = new byte[2];
+		fis.read(ignoreBytes); // "B", "Z" bytes from commandline tools
+		br = new BufferedReader(new InputStreamReader(new CBZip2InputStream(fis)));
+
+	}
+
+	public boolean readNext(WikipediaPage page) throws IOException {
+		String s = null;
+		StringBuffer sb = new StringBuffer(DEFAULT_STRINGBUFFER_CAPACITY);
+
+		while ((s = br.readLine()) != null) {
+			if (s.endsWith("<page>"))
+				break;
+		}
+
+		if (s == null) {
+			fis.close();
+			br.close();
+			return false;
+		}
+
+		sb.append(s + "\n");
+
+		while ((s = br.readLine()) != null) {
+			sb.append(s + "\n");
+
+			if (s.endsWith("</page>"))
+				break;
+		}
+
+		WikipediaPage.readPage(page, sb.toString());
+
+		return true;
+	}
+
+}
