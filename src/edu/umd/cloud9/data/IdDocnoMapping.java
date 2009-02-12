6cd7704973b1c46978d5cf4154face0d73c32cae
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-02-12 18:46:24 +0000

    tweaked API

--- src/edu/umd/cloud9/data/IdDocnoMapping.java
+++ src/edu/umd/cloud9/data/IdDocnoMapping.java
@@ -2,8 +2,11 @@ package edu.umd.cloud9.data;
 
 import java.io.IOException;
 
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
 public interface IdDocnoMapping {
 	public int getDocno(String docid);
 
-	public void loadMapping(String f) throws IOException;
+	public void loadMapping(Path f, FileSystem fs) throws IOException;
 }
