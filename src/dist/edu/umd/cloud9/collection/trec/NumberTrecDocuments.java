8adbbf95d9d316f144ebd567925cc5166dd22d82
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-05-06 00:41:55 +0000

    program to map TREC docids to sequential docnos

---
+++ src/dist/edu/umd/cloud9/collection/trec/NumberTrecDocuments.java
@@ -0,0 +1,98 @@
+package edu.umd.cloud9.collection.trec;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextOutputFormat;
+
+public class NumberTrecDocuments {
+
+	protected static enum Count {
+		DOCS
+	};
+
+	private static class MyMapper extends MapReduceBase implements
+			Mapper<LongWritable, TrecDocument, Text, IntWritable> {
+
+		private final static Text sText = new Text();
+		private final static IntWritable sInt = new IntWritable(1);
+
+		public void map(LongWritable key, TrecDocument doc,
+				OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+			reporter.incrCounter(Count.DOCS, 1);
+
+			sText.set(doc.getDocid());
+			output.collect(sText, sInt);
+		}
+	}
+
+	private static class MyReducer extends MapReduceBase implements
+			Reducer<Text, IntWritable, Text, IntWritable> {
+
+		private final static IntWritable sCnt = new IntWritable(0);
+
+		public void reduce(Text key, Iterator<IntWritable> values,
+				OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+			output.collect(key, sCnt);
+			sCnt.set(sCnt.get() + 1);
+		}
+	}
+
+	private NumberTrecDocuments() {
+	}
+
+	public static void main(String[] args) throws IOException {
+		if (args.length != 4) {
+			System.out.println("usage: [xml-dump] [output-dir] [output-file] [num-mappers]");
+			System.exit(-1);
+		}
+
+		String inputPath = args[0];
+		String outputPath = args[1];
+		String outputFile = args[2];
+		int mapTasks = Integer.parseInt(args[3]);
+
+		System.out.println("input: " + inputPath);
+		System.out.println("output: " + outputPath);
+		System.out.println("number of mappers: " + mapTasks);
+
+		JobConf conf = new JobConf(NumberTrecDocuments.class);
+		conf.setJobName("NumberTrecDocuments");
+
+		conf.setNumMapTasks(mapTasks);
+		conf.setNumReduceTasks(1);
+
+		FileInputFormat.setInputPaths(conf, new Path(inputPath));
+		FileOutputFormat.setOutputPath(conf, new Path(outputPath));
+		FileOutputFormat.setCompressOutput(conf, false);
+
+		conf.setInputFormat(TrecDocumentInputFormat.class);
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(IntWritable.class);
+		conf.setOutputFormat(TextOutputFormat.class);
+
+		conf.setMapperClass(MyMapper.class);
+		conf.setReducerClass(MyReducer.class);
+
+		// delete the output directory if it exists already
+		FileSystem.get(conf).delete(new Path(outputPath), true);
+
+		JobClient.runJob(conf);
+
+		TrecDocnoMapping.writeDocnoData(outputPath + "/part-00000", outputFile);
+	}
+}
