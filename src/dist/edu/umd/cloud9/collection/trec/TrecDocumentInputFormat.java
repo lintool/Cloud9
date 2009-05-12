4ba19b63405ee3a7398743a045a66c79a52322ce
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-05-12 04:47:41 +0000

    added javadoc, license info

--- src/dist/edu/umd/cloud9/collection/trec/TrecDocumentInputFormat.java
+++ src/dist/edu/umd/cloud9/collection/trec/TrecDocumentInputFormat.java
@@ -1,3 +1,19 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package edu.umd.cloud9.collection.trec;
 
 import java.io.IOException;
@@ -15,13 +31,13 @@ import edu.umd.cloud9.collection.XMLInputFormat;
 import edu.umd.cloud9.collection.XMLInputFormat.XMLRecordReader;
 
 /**
- * InputFormat for reading the TREC collection
+ * InputFormat for reading the TREC collection.
  */
 public class TrecDocumentInputFormat extends IndexableFileInputFormat<LongWritable, TrecDocument> {
 
-	public void configure(JobConf conf) {
-	}
-
+	/**
+	 * Returns a RecordReader for this InputFormat.
+	 */
 	public RecordReader<LongWritable, TrecDocument> getRecordReader(InputSplit inputSplit,
 			JobConf conf, Reporter reporter) throws IOException {
 		return new TrecDocumentRecordReader((FileSplit) inputSplit, conf);
@@ -37,6 +53,9 @@ public class TrecDocumentInputFormat extends IndexableFileInputFormat<LongWritab
 		private Text mText = new Text();
 		private LongWritable mLong = new LongWritable();
 
+		/**
+		 * Creates a TrecDocumentRecordReader.
+		 */
 		public TrecDocumentRecordReader(FileSplit split, JobConf conf) throws IOException {
 			conf.set(XMLInputFormat.START_TAG_KEY, TrecDocument.XML_START_TAG);
 			conf.set(XMLInputFormat.END_TAG_KEY, TrecDocument.XML_END_TAG);
@@ -44,6 +63,9 @@ public class TrecDocumentInputFormat extends IndexableFileInputFormat<LongWritab
 			mReader = new XMLRecordReader(split, conf);
 		}
 
+		/**
+		 * Returns the next key-value pair.
+		 */
 		public boolean next(LongWritable key, TrecDocument value) throws IOException {
 			if (mReader.next(mLong, mText) == false)
 				return false;
@@ -52,22 +74,37 @@ public class TrecDocumentInputFormat extends IndexableFileInputFormat<LongWritab
 			return true;
 		}
 
+		/**
+		 * Creates an object for the key.
+		 */
 		public LongWritable createKey() {
 			return new LongWritable();
 		}
 
+		/**
+		 * Creates an object for the value.
+		 */
 		public TrecDocument createValue() {
 			return new TrecDocument();
 		}
 
+		/**
+		 * Returns the current position in the input.
+		 */
 		public long getPos() throws IOException {
 			return mReader.getPos();
 		}
 
+		/**
+		 * Closes this InputSplit.
+		 */
 		public void close() throws IOException {
 			mReader.close();
 		}
 
+		/**
+		 * Returns progress on how much input has been consumed.
+		 */
 		public float getProgress() throws IOException {
 			return ((float) (mReader.getPos() - mReader.getStart()))
 					/ ((float) (mReader.getEnd() - mReader.getStart()));
