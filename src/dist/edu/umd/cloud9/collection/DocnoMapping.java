e926b3d8fc9112188bcfc1acc8053eeba0b86bcf
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-05-12 13:02:07 +0000

    added javadoc

--- src/dist/edu/umd/cloud9/collection/DocnoMapping.java
+++ src/dist/edu/umd/cloud9/collection/DocnoMapping.java
@@ -1,3 +1,19 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package edu.umd.cloud9.collection;
 
 import java.io.IOException;
@@ -7,36 +23,50 @@ import org.apache.hadoop.fs.Path;
 
 /**
  * <p>
- * Interface for an object that maps between <code>docid</code>s and
- * <code>docno</code>s. A <code>docid</code> is a globally-unique
- * identifier (String) for a document in the collection. For many types of
- * processing, documents in the collection must be sequentially numbered; thus,
- * each document in the collection must be assigned a unique integer identifier,
- * which is its <code>docno</code>.
+ * Interface for an object that maintains the mapping between docids and docnos.
+ * A docid is a globally-unique String identifier for a document in the
+ * collection. For many types of information retrieval algorithms, documents in
+ * the collection must be sequentially numbered; thus, each document in the
+ * collection must be assigned a unique integer identifier, which is its docno.
+ * Typically, the docid to docno mappings are stored in a mappings file, which
+ * is loaded into memory by concrete objects implementing this interface.
  * </p>
  * 
  * <p>
  * Unless there are compelling reasons otherwise, it is preferable to start
- * numbering <code>docno</code>s from one instead of zero. This is because
- * zero cannot be represented in many common compression schemes that are used
- * in information retrieval (e.g., Golomb codes).
+ * numbering docnos from one instead of zero. This is because zero cannot be
+ * represented in many common compression schemes that are used in information
+ * retrieval (e.g., Golomb codes).
  * </p>
  */
 public interface DocnoMapping {
 
 	/**
-	 * Returns the <code>docno<code> for a particular <code>docid</code>.
-	 * @param docid the <code>docid</code>
-	 * @return the <code>docno<code> for the <code>docid</code>
+	 * Returns the docno for a particular docid.
+	 * 
+	 * @param docid
+	 *            the docid
+	 * @return the docno for the docid
 	 */
 	public int getDocno(String docid);
 
 	/**
-	 * Returns the <code>docid<code> for a particular <code>docno</code>.
-	 * @param docno the <code>docno</code>
-	 * @return the <code>docid<code> for the <code>docno</code>
+	 * Returns the docid for a particular docno.
+	 * 
+	 * @param docno
+	 *            the docno
+	 * @return the docid for the docno
 	 */
 	public String getDocid(int docno);
 
-	public void loadMapping(Path f, FileSystem fs) throws IOException;
+	/**
+	 * Loads a mapping file containing the docid to docno mappings.
+	 * 
+	 * @param p
+	 *            path to the mappings file
+	 * @param fs
+	 *            appropriate FileSystem
+	 * @throws IOException
+	 */
+	public void loadMapping(Path p, FileSystem fs) throws IOException;
 }
--- src/dist/edu/umd/cloud9/collection/Indexable.java
+++ src/dist/edu/umd/cloud9/collection/Indexable.java
@@ -6,7 +6,15 @@ import org.apache.hadoop.io.Writable;
  * Interface for a document that can be indexed.
  */
 public interface Indexable extends Writable {
+
+	/**
+	 * Returns the globally-unique String identifier of the document within the
+	 * collection.
+	 */
 	public String getDocid();
 
+	/**
+	 * Returns the content of the document.
+	 */
 	public String getContent();
 }
