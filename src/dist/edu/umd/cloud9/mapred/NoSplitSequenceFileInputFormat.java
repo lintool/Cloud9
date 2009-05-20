6da6a9346d6d6be152aaebb01c1fc4060eb52ad4
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-05-20 14:44:25 +0000

    a SequenceFileInputFormat that doesn't split files

---
+++ src/dist/edu/umd/cloud9/mapred/NoSplitSequenceFileInputFormat.java
@@ -0,0 +1,15 @@
+package edu.umd.cloud9.mapred;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapred.SequenceFileInputFormat;
+
+public class NoSplitSequenceFileInputFormat<K extends WritableComparable, V extends Writable>
+		extends SequenceFileInputFormat<K, V> {
+
+	protected boolean isSplitable(FileSystem fs, Path filename) {
+		return false;
+	}
+}
