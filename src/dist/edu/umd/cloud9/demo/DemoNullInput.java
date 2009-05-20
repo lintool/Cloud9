008050988132204a71f92014065eae4d16ebbeaa
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-05-20 03:25:10 +0000

    demo for running a MapReduce job that has neither input nor output

---
+++ src/dist/edu/umd/cloud9/demo/DemoNullInput.java
@@ -0,0 +1,59 @@
+package edu.umd.cloud9.demo;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.log4j.Logger;
+
+import edu.umd.cloud9.mapred.NullInputFormat;
+import edu.umd.cloud9.mapred.NullOutputFormat;
+
+public class DemoNullInput {
+
+	private static final Logger sLogger = Logger.getLogger(DemoNullInput.class);
+
+	private static class MyMapper extends MapReduceBase implements
+			Mapper<NullWritable, NullWritable, NullWritable, NullWritable> {
+
+		public void map(NullWritable key, NullWritable value,
+				OutputCollector<NullWritable, NullWritable> output, Reporter reporter)
+				throws IOException {
+
+			sLogger.info("Counting to 10:");
+			for (int i = 0; i < 10; i++) {
+				sLogger.info(i + 1 + "...");
+				try {
+					Thread.sleep(10000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	protected DemoNullInput() {
+	}
+
+	/**
+	 * Runs the demo.
+	 */
+	public static void main(String[] args) throws IOException {
+		JobConf conf = new JobConf(DemoNullInput.class);
+		conf.setJobName("DemoNullInput");
+
+		conf.setNumMapTasks(10);
+		conf.setNumReduceTasks(0);
+
+		conf.setInputFormat(NullInputFormat.class);
+		conf.setOutputFormat(NullOutputFormat.class);
+		conf.setMapperClass(MyMapper.class);
+
+		JobClient.runJob(conf);
+	}
+}
