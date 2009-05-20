450941df130c9a4d190c3e0620565107266f89d0
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-05-20 14:16:35 +0000

    added main to manipulate properties

--- src/dist/edu/umd/cloud9/util/FSProperty.java
+++ src/dist/edu/umd/cloud9/util/FSProperty.java
@@ -2,6 +2,7 @@ package edu.umd.cloud9.util;
 
 import java.io.IOException;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
@@ -93,4 +94,53 @@ public class FSProperty {
 		}
 	}
 
+	public static void main(String[] args) throws IOException {
+		if (args.length < 3) {
+			System.out.println("args: (read|write) (int|long|float|String) [file] (value)");
+			System.exit(-1);
+		}
+
+		FileSystem fs = FileSystem.get(new Configuration());
+		if (args[0].equals("read")) {
+			if (args[1].equals("int")) {
+				System.out.println("reading int from " + args[2]);
+				System.out.println(readInt(fs, args[2]));
+			} else if (args[1].equals("float")) {
+				System.out.println("reading float from " + args[2]);
+				System.out.println(readFloat(fs, args[2]));
+			} else if (args[1].equals("long")) {
+				System.out.println("reading long from " + args[2]);
+				System.out.println(readLong(fs, args[2]));
+			} else if (args[1].equals("String")) {
+				System.out.println("reading String from " + args[2]);
+				System.out.println(readString(fs, args[2]));
+			} else {
+				System.out.println("unknown read type");
+				System.out.println("args: read (int|long|float|String) [file]");
+				System.exit(-1);
+			}
+		} else if (args[0].equals("write")) {
+			if (args[1].equals("int")) {
+				int i = Integer.parseInt(args[3]);
+				System.out.println("writing int \"" + i + "\" to " + args[2]);
+				writeInt(fs, args[2], i);
+			} else if (args[1].equals("float")) {
+				float i = Float.parseFloat(args[3]);
+				System.out.println("writing float \"" + i + "\" to " + args[2]);
+				writeFloat(fs, args[2], i);
+			} else if (args[1].equals("long")) {
+				long i = Long.parseLong(args[3]);
+				System.out.println("writing long \"" + i + "\" to " + args[2]);
+				writeLong(fs, args[2], i);
+			} else if (args[1].equals("String")) {
+				System.out.println("writing String \"" + args[3] + "\" to " + args[2]);
+				writeString(fs, args[2], args[3]);
+			} else {
+				System.out.println("unknown write type");
+				System.out.println("args: write (int|long|float|String) [file] [value]");
+				System.exit(-1);
+			}
+		}
+
+	}
 }
