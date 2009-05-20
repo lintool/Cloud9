fcf7d43980cf8f88fee8b5a204bdbc607164030f
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-05-20 22:26:06 +0000

    simple static method to get string representation of stack trace

---
+++ src/dist/edu/umd/cloud9/util/ExceptionUtils.java
@@ -0,0 +1,15 @@
+package edu.umd.cloud9.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class ExceptionUtils {
+
+	public static String getStackTrace(Exception e) {
+		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+		PrintStream stream = new PrintStream(bytes);
+		e.printStackTrace(stream);
+
+		return new String(bytes.toByteArray());
+	}
+}
