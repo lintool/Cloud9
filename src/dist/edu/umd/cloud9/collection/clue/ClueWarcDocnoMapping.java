939bb937d14c7f032cfb9142756aa642b88f0c38
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-06-15 23:55:07 +0000

    implemented getDocid

--- src/dist/edu/umd/cloud9/collection/clue/ClueWarcDocnoMapping.java
+++ src/dist/edu/umd/cloud9/collection/clue/ClueWarcDocnoMapping.java
@@ -1,6 +1,8 @@
 package edu.umd.cloud9.collection.clue;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -8,7 +10,8 @@ import org.apache.hadoop.io.Text;
 
 import edu.umd.cloud9.collection.DocnoMapping;
 import edu.umd.cloud9.util.FSLineReader;
-import edu.umd.cloud9.util.HMapKI;
+import edu.umd.cloud9.util.MapKI;
+import edu.umd.cloud9.util.OHMapKI;
 
 /**
  * <p>
@@ -26,8 +29,11 @@ import edu.umd.cloud9.util.HMapKI;
  */
 public class ClueWarcDocnoMapping implements DocnoMapping {
 
-	private static int[] sOffets = new int[13217];
-	private static final HMapKI<String> sSubDirMapping = new HMapKI<String>();
+	private static final int[] sOffets = new int[13217];
+	private static final OHMapKI<String> sSubDirMapping = new OHMapKI<String>();
+
+	private static final NumberFormat sFormatW2 = new DecimalFormat("00");
+	private static final NumberFormat sFormatW5 = new DecimalFormat("00000");
 
 	/**
 	 * Creates a <code>ClueWarcDocnoMapping</code> object
@@ -52,7 +58,26 @@ public class ClueWarcDocnoMapping implements DocnoMapping {
 	}
 
 	public String getDocid(int docno) {
-		throw new UnsupportedOperationException();
+		int i = 0;
+		for (i = 0; i < sOffets.length; i++) {
+			if (docno < sOffets[i])
+				break;
+		}
+		i--;
+
+		//System.out.println("offset = " + i + ", " + sOffets[i]);
+
+		String docid = null;
+		for (MapKI.Entry<String> e : sSubDirMapping.getEntriesSortedByValue()) {
+			if (e.getValue() <= i) {
+				//System.out.println(e.getKey() + "\t" + e.getValue());
+				docid = "clueweb09-" + e.getKey() + "-" + sFormatW2.format(i - e.getValue()) + "-"
+						+ sFormatW5.format(docno - sOffets[i]);
+				break;
+			}
+		}
+
+		return docid;
 	}
 
 	public void loadMapping(Path p, FileSystem fs) throws IOException {
--- src/dist/edu/umd/cloud9/collection/clue/ClueWarcDocnoMappingTest.java
+++ src/dist/edu/umd/cloud9/collection/clue/ClueWarcDocnoMappingTest.java
@@ -11,14 +11,13 @@ import org.junit.Test;
 public class ClueWarcDocnoMappingTest {
 
 	@Test
-	public void testSerialize1() throws Exception {
+	public void testDocidToDocno() throws Exception {
 		ClueWarcDocnoMapping mapping = new ClueWarcDocnoMapping();
 
 		mapping.loadMapping(new Path("etc/Clue-English-docno.mapping"), FileSystem
 				.get(new Configuration()));
 
 		assertEquals(1, mapping.getDocno("clueweb09-en0000-00-00000"));
-
 		assertEquals(28335180, mapping.getDocno("clueweb09-en0007-91-00000"));
 		assertEquals(28378418, mapping.getDocno("clueweb09-en0007-91-43238"));
 		assertEquals(28378419, mapping.getDocno("clueweb09-en0007-92-00000"));
@@ -26,6 +25,23 @@ public class ClueWarcDocnoMappingTest {
 		assertEquals(50220424, mapping.getDocno("clueweb09-en0012-00-00000"));
 		assertEquals(140950355, mapping.getDocno("clueweb09-en0038-18-42833"));
 		assertEquals(503903810, mapping.getDocno("clueweb09-en0133-99-55739"));
+	}
+
+	@Test
+	public void testDocnoToDocid() throws Exception {
+		ClueWarcDocnoMapping mapping = new ClueWarcDocnoMapping();
+
+		mapping.loadMapping(new Path("etc/Clue-English-docno.mapping"), FileSystem
+				.get(new Configuration()));
+
+		assertEquals("clueweb09-en0000-00-00000", mapping.getDocid(1));
+		assertEquals("clueweb09-en0007-91-00000", mapping.getDocid(28335180));
+		assertEquals("clueweb09-en0007-91-43238", mapping.getDocid(28378418));
+		assertEquals("clueweb09-en0007-92-00000", mapping.getDocid(28378419));
+		assertEquals("clueweb09-enwp00-00-00000", mapping.getDocid(44262895));
+		assertEquals("clueweb09-en0012-00-00000", mapping.getDocid(50220424));
+		assertEquals("clueweb09-en0038-18-42833", mapping.getDocid(140950355));
+		assertEquals("clueweb09-en0133-99-55739", mapping.getDocid(503903810));
 	}
 
 	public static junit.framework.Test suite() {
