48cc55a9861b0554a34d0739f9bbcdde3f6c3157
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-07-11 22:05:22 +0000

    +boilerplate refactoring into Tool

--- src/dist/edu/umd/cloud9/collection/aquaint2/DemoCountAquaint2Documents.java
+++ src/dist/edu/umd/cloud9/collection/aquaint2/DemoCountAquaint2Documents.java
@@ -1,7 +1,24 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package edu.umd.cloud9.collection.aquaint2;
 
 import java.io.IOException;
 
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -15,8 +32,10 @@ import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class DemoCountAquaint2Documents {
+public class DemoCountAquaint2Documents extends Configured implements Tool {
 
 	private static enum Count {
 		DOCS
@@ -30,20 +49,36 @@ public class DemoCountAquaint2Documents {
 		}
 	}
 
-	private DemoCountAquaint2Documents() {
+	/**
+	 * Creates an instance of this tool.
+	 */
+	public DemoCountAquaint2Documents() {
 	}
 
+	private static int printUsage() {
+		System.out.println("usage: [input] [output-dir] [mappings-file] [num-mappers]");
+		ToolRunner.printGenericCommandUsage(System.out);
+		return -1;
+	}
+	
 	/**
-	 * Runs the demo.
+	 * Runs this tool.
 	 */
-	public static void main(String[] args) throws IOException {
-		String inputPath = "/umd/collections/aquaint2/";
-		int mapTasks = 200;
+	public int run(String[] args) throws Exception {
+		if (args.length != 4) {
+			printUsage();
+			return -1;
+		}
 
-		System.out.println("input: " + inputPath);
+		String inputPath = args[0];
+		String outputPath = args[1];
+		String mappingFile = args[2];
+		int mapTasks = Integer.parseInt(args[3]);
 
-		long r = System.currentTimeMillis();
-		String outputPath = "/tmp/" + r;
+		System.out.println("input dir: " + inputPath);
+		System.out.println("output dir: " + outputPath);
+		System.out.println("mapping file: " + mappingFile);
+		System.out.println("number of mappers: " + mapTasks);
 
 		JobConf conf = new JobConf(DemoCountAquaint2Documents.class);
 		conf.setJobName("DemoCountAquaint2Documents");
@@ -68,5 +103,7 @@ public class DemoCountAquaint2Documents {
 
 		// clean up
 		FileSystem.get(conf).delete(new Path(outputPath), true);
+		
+		return 0;
 	}
 }
--- src/dist/edu/umd/cloud9/collection/aquaint2/NumberAquaint2Documents.java
+++ src/dist/edu/umd/cloud9/collection/aquaint2/NumberAquaint2Documents.java
@@ -1,8 +1,26 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package edu.umd.cloud9.collection.aquaint2;
 
 import java.io.IOException;
 import java.util.Iterator;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -18,10 +36,12 @@ import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class NumberAquaint2Documents {
+public class NumberAquaint2Documents extends Configured implements Tool {
 
-	protected static enum Count {
+	private static enum Count {
 		DOCS
 	};
 
@@ -52,13 +72,25 @@ public class NumberAquaint2Documents {
 		}
 	}
 
-	private NumberAquaint2Documents() {
+	/**
+	 * Creates an instance of this tool.
+	 */
+	public NumberAquaint2Documents() {
 	}
 
-	public static void main(String[] args) throws IOException {
+	private static int printUsage() {
+		System.out.println("usage: [input-path] [output-path] [output-file] [num-mappers]");
+		ToolRunner.printGenericCommandUsage(System.out);
+		return -1;
+	}
+
+	/**
+	 * Runs this tool.
+	 */
+	public int run(String[] args) throws Exception {
 		if (args.length != 4) {
-			System.out.println("usage: [xml-dump] [output-dir] [output-file] [num-mappers]");
-			System.exit(-1);
+			printUsage();
+			return -1;
 		}
 
 		String inputPath = args[0];
@@ -94,5 +126,16 @@ public class NumberAquaint2Documents {
 		JobClient.runJob(conf);
 
 		Aquaint2DocnoMapping.writeDocnoData(outputPath + "/part-00000", outputFile);
+		
+		return 0;
+	}
+	
+	/**
+	 * Dispatches command-line arguments to the tool via the
+	 * <code>ToolRunner</code>.
+	 */
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new Configuration(), new NumberAquaint2Documents(), args);
+		System.exit(res);
 	}
 }
