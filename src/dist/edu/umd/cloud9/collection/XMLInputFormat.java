a0dd15c67ff5a5b69f92a8f176bece989676913c
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-08-02 14:10:55 +0000

    refactored to work with either compressed or uncompressed XML files

--- src/dist/edu/umd/cloud9/collection/XMLInputFormat.java
+++ src/dist/edu/umd/cloud9/collection/XMLInputFormat.java
@@ -16,23 +16,28 @@
 
 package edu.umd.cloud9.collection;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.Seekable;
 import org.apache.hadoop.io.DataOutputBuffer;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionCodecFactory;
 import org.apache.hadoop.mapred.FileSplit;
 import org.apache.hadoop.mapred.InputSplit;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.RecordReader;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.log4j.Logger;
 
 // solution for reading XML files, posted to the Hadoop users mailing list
-// Re: map/reduce function on xml string  - Colin Evans-2 Mar 04, 2008; 02:27pm
+// Re: map/reduce function on xml string - Colin Evans-2 Mar 04, 2008; 02:27pm
 public class XMLInputFormat extends TextInputFormat {
 	public static final String START_TAG_KEY = "xmlinput.start";
 	public static final String END_TAG_KEY = "xmlinput.end";
@@ -47,15 +52,18 @@ public class XMLInputFormat extends TextInputFormat {
 	}
 
 	public static class XMLRecordReader implements RecordReader<LongWritable, Text> {
+		private static final Logger sLogger = Logger.getLogger(XMLRecordReader.class);
+				
 		private byte[] startTag;
 		private byte[] endTag;
 		private long start;
 		private long end;
-		private FSDataInputStream fsin;
+		private long pos;
+		private DataInputStream fsin = null;
 		private DataOutputBuffer buffer = new DataOutputBuffer();
 
 		private long recordStartPos;
-		
+
 		public XMLRecordReader(FileSplit split, JobConf jobConf) throws IOException {
 			if (jobConf.get(START_TAG_KEY) == null || jobConf.get(END_TAG_KEY) == null)
 				throw new RuntimeException("Error! XML start and end tags unspecified!");
@@ -63,20 +71,46 @@ public class XMLInputFormat extends TextInputFormat {
 			startTag = jobConf.get(START_TAG_KEY).getBytes("utf-8");
 			endTag = jobConf.get(END_TAG_KEY).getBytes("utf-8");
 
-			// open the file and seek to the start of the split
 			start = split.getStart();
-			end = start + split.getLength();
 			Path file = split.getPath();
+
+			CompressionCodecFactory compressionCodecs = new CompressionCodecFactory(jobConf);
+			CompressionCodec codec = compressionCodecs.getCodec(file);
+			
 			FileSystem fs = file.getFileSystem(jobConf);
-			fsin = fs.open(split.getPath());
-			fsin.seek(start);
+
+			if (codec != null) {
+				sLogger.info("Reading compressed file...");
+
+				//InputStream tempStream = codec.createInputStream(fileIn);
+				//fsin = new DataInputStream(tempStream);
+
+				fsin = new DataInputStream(codec.createInputStream(fs.open(file)));
+				
+				end = Long.MAX_VALUE;
+			} else {
+				sLogger.info("Reading uncompressed file...");
+
+				FSDataInputStream fileIn = fs.open(file);
+
+				fileIn.seek(start);
+				fsin = fileIn;
+				
+				end = start + split.getLength();
+			}
+
 			recordStartPos = start;
+
+			// Because input streams of gzipped files are not seekable
+			// (specifically, do not support getPos), we need to keep
+			// track of bytes consumed ourselves.
+			pos = start;
 		}
 
-		public boolean next(LongWritable key, Text value) throws IOException {
-			if (fsin.getPos() < end) {
+		public boolean next(LongWritable key, Text value) throws IOException {			
+			if (pos < end) {
 				if (readUntilMatch(startTag, false)) {
-					recordStartPos = fsin.getPos()- startTag.length;
+					recordStartPos = pos - startTag.length;
 
 					try {
 						buffer.write(startTag);
@@ -86,6 +120,21 @@ public class XMLInputFormat extends TextInputFormat {
 							return true;
 						}
 					} finally {
+						// Because input streams of gzipped files are not
+						// seekable (specifically, do not support getPos), we
+						// need to keep track of bytes consumed ourselves.
+
+						// This is a sanity check to make sure our internal
+						// computation of bytes consumed is accurate. This
+						// should be removed later for efficiency once we
+						// confirm that this code works correctly.
+
+						if (fsin instanceof Seekable) {
+							if (pos != ((Seekable) fsin).getPos()) {
+								throw new RuntimeException("bytes consumed error!");
+							}
+						}
+
 						buffer.reset();
 					}
 				}
@@ -110,7 +159,7 @@ public class XMLInputFormat extends TextInputFormat {
 		}
 
 		public long getPos() throws IOException {
-			return fsin.getPos();
+			return pos;
 		}
 
 		public void close() throws IOException {
@@ -118,13 +167,16 @@ public class XMLInputFormat extends TextInputFormat {
 		}
 
 		public float getProgress() throws IOException {
-			return ((float) (fsin.getPos() - start)) / ((float) (end - start));
+			return ((float) (pos - start)) / ((float) (end - start));
 		}
 
 		private boolean readUntilMatch(byte[] match, boolean withinBlock) throws IOException {
 			int i = 0;
 			while (true) {
 				int b = fsin.read();
+				// increment position (bytes consumed)
+				pos++;
+
 				// end of file:
 				if (b == -1)
 					return false;
@@ -140,7 +192,7 @@ public class XMLInputFormat extends TextInputFormat {
 				} else
 					i = 0;
 				// see if we've passed the stop point:
-				if (!withinBlock && i == 0 && fsin.getPos() >= end)
+				if (!withinBlock && i == 0 && pos >= end)
 					return false;
 			}
 		}
