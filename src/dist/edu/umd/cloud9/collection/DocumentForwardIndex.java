da2bda441fccc26d492fcc13e267aba72ee5d6db
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-09-19 01:38:42 +0000

    refactoring; ClueWarcForwardIndex implements DocumentForwardIndex

---
+++ src/dist/edu/umd/cloud9/collection/DocumentForwardIndex.java
@@ -0,0 +1,31 @@
+package edu.umd.cloud9.collection;
+
+import java.io.IOException;
+
+/**
+ * Interface for a document forward index.
+ * 
+ * @author Jimmy Lin
+ * 
+ * @param <T>
+ *            type of document
+ */
+public interface DocumentForwardIndex<T extends Indexable> {
+
+	public int getDocno(String docid);
+
+	public String getDocid(int docno);
+
+	public T getDocument(int docno);
+	
+	public T getDocument(String docid);
+	
+	public int getFirstDocno();
+	
+	public int getLastDocno();
+		
+	public void loadIndex(String indexFile, String collectionPath, String mappingDataFile)
+			throws IOException;
+
+	public String getContentType();
+}
--- src/dist/edu/umd/cloud9/collection/clue/ClueWarcForwardIndex.java
+++ src/dist/edu/umd/cloud9/collection/clue/ClueWarcForwardIndex.java
@@ -12,33 +12,37 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.log4j.Logger;
 
-public class ClueWarcForwardIndex {
+import edu.umd.cloud9.collection.DocumentForwardIndex;
+
+public class ClueWarcForwardIndex implements DocumentForwardIndex<ClueWarcRecord> {
 
 	private static final Logger sLogger = Logger.getLogger(ClueWarcForwardIndex.class);
 
-	Configuration conf;
-	FileSystem fs;
+	private Configuration mConf;
+	private FileSystem mFS;
 
 	private int[] mDocnos;
 	private int[] mOffsets;
 	private short[] mFileno;
-
 	private String mCollectionPath;
 
 	private ClueWarcDocnoMapping mDocnoMapping = new ClueWarcDocnoMapping();
 
-	public ClueWarcForwardIndex(String indexFile, String collectionPath, String mappingDataFile)
+	public ClueWarcForwardIndex() {
+	}
+
+	public void loadIndex(String indexFile, String collectionPath, String mappingDataFile)
 			throws IOException {
 		sLogger.info("Loading forward index: " + indexFile);
 
 		mCollectionPath = collectionPath;
 
-		conf = new Configuration();
-		fs = FileSystem.get(conf);
+		mConf = new Configuration();
+		mFS = FileSystem.get(mConf);
 
-		mDocnoMapping.loadMapping(new Path(mappingDataFile), fs);
+		mDocnoMapping.loadMapping(new Path(mappingDataFile), mFS);
 
-		FSDataInputStream in = fs.open(new Path(indexFile));
+		FSDataInputStream in = mFS.open(new Path(indexFile));
 
 		int blocks = in.readInt();
 
@@ -59,7 +63,8 @@ public class ClueWarcForwardIndex {
 		in.close();
 	}
 
-	public ClueWarcRecord getDocno(int docno) {
+	public ClueWarcRecord getDocument(int docno) {
+		long start = System.currentTimeMillis();
 		int idx = Arrays.binarySearch(mDocnos, docno);
 
 		if (idx < 0)
@@ -68,11 +73,11 @@ public class ClueWarcForwardIndex {
 		DecimalFormat df = new DecimalFormat("00000");
 		String file = mCollectionPath + "/part-" + df.format(mFileno[idx]);
 
-		sLogger.info("fetching " + docno + ": seeking to " + mOffsets[idx] + " at " + file);
+		sLogger.info("fetching docno " + docno + ": seeking to " + mOffsets[idx] + " at " + file);
 
 		try {
 
-			SequenceFile.Reader reader = new SequenceFile.Reader(fs, new Path(file), conf);
+			SequenceFile.Reader reader = new SequenceFile.Reader(mFS, new Path(file), mConf);
 
 			IntWritable key = new IntWritable();
 			ClueWarcRecord value = new ClueWarcRecord();
@@ -88,6 +93,9 @@ public class ClueWarcForwardIndex {
 			reader.getCurrentValue(value);
 			reader.close();
 
+			long duration = System.currentTimeMillis() - start;
+
+			sLogger.info(" docno " + docno + " fetched in " + duration + "ms");
 			return value;
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -96,33 +104,79 @@ public class ClueWarcForwardIndex {
 		return null;
 	}
 
-	public ClueWarcRecord getDocid(String docid) {
-		return getDocno(mDocnoMapping.getDocno(docid));
+	public ClueWarcRecord getDocument(String docid) {
+		return getDocument(mDocnoMapping.getDocno(docid));
+	}
+
+	public int getDocno(String docid) {
+		return mDocnoMapping.getDocno(docid);
+	}
+
+	public String getDocid(int docno) {
+		return mDocnoMapping.getDocid(docno);
+	}
+
+	public int getFirstDocno() {
+		return mDocnos[0];
+	}
+
+	private int mLastDocno = -1;
+
+	public int getLastDocno() {
+		if (mLastDocno != -1)
+			return mLastDocno;
+
+		// find the last entry, and then see all the way to the end of the
+		// collection
+		int idx = mDocnos.length - 1;
+
+		DecimalFormat df = new DecimalFormat("00000");
+		String file = mCollectionPath + "/part-" + df.format(mFileno[idx]);
+
+		try {
+			SequenceFile.Reader reader = new SequenceFile.Reader(mFS, new Path(file), mConf);
+			IntWritable key = new IntWritable();
+
+			reader.seek(mOffsets[idx]);
+
+			while (reader.next(key))
+				;
+			mLastDocno = key.get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return mLastDocno;
+	}
+
+	public String getContentType() {
+		return "text/html";
 	}
 
 	public static void main(String[] args) throws Exception {
-		ClueWarcForwardIndex f = new ClueWarcForwardIndex("/user/jimmylin/en.01.findex",
-				"/umd/collections/ClueWeb09.repacked/en.01/",
+		ClueWarcForwardIndex f = new ClueWarcForwardIndex();
+
+		f.loadIndex("/user/jimmylin/en.01.findex", "/umd/collections/ClueWeb09.repacked/en.01/",
 				"/umd/collections/ClueWeb09.repacked/docno-mapping.dat");
 
 		ClueWarcRecord record;
 
-		record = f.getDocno(1000);
+		record = f.getDocument(1000);
 		System.out.println(record.getDocid());
 
-		record = f.getDocno(999964);
+		record = f.getDocument(999964);
 		System.out.println(record.getDocid());
 
-		record = f.getDocno(1000000);
-		System.out.println(record.getDocid());
-		
-		record = f.getDocid("clueweb09-en0000-00-00999");
+		record = f.getDocument(1000000);
 		System.out.println(record.getDocid());
 
-		record = f.getDocid("clueweb09-en0000-29-13277");
+		record = f.getDocument("clueweb09-en0000-00-00999");
 		System.out.println(record.getDocid());
 
-		record = f.getDocid("clueweb09-en0000-29-13313");
+		record = f.getDocument("clueweb09-en0000-29-13277");
+		System.out.println(record.getDocid());
+
+		record = f.getDocument("clueweb09-en0000-29-13313");
 		System.out.println(record.getDocid());
 	}
 }
