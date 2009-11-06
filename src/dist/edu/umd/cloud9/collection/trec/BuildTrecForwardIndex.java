cb6c080f23cbd3b2920479dc940d165340edbbe8
jimmylin <jimmylin@706a6fc6-e23c-0410-9c2f-a234a775544c>
2009-11-06 22:00:29 +0000

    forward index infrastructure for TREC

---
+++ src/dist/edu/umd/cloud9/collection/trec/BuildTrecForwardIndex.java
@@ -0,0 +1,194 @@
+package edu.umd.cloud9.collection.trec;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.Counters;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.RunningJob;
+import org.apache.hadoop.mapred.lib.IdentityReducer;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+
+import edu.umd.cloud9.collection.DocnoMapping;
+import edu.umd.cloud9.util.FSLineReader;
+
+/**
+ * <p>
+ * Tool for building a document forward index for TREC collections.
+ * </p>
+ * 
+ * @author Jimmy Lin
+ * 
+ */
+public class BuildTrecForwardIndex extends Configured implements Tool {
+
+	private static final Logger sLogger = Logger.getLogger(BuildTrecForwardIndex.class);
+
+	private static enum Count {
+		DOCS
+	};
+
+	private static class MyMapper extends MapReduceBase implements
+			Mapper<LongWritable, TrecDocument, IntWritable, Text> {
+
+		private final static IntWritable sInt = new IntWritable(1);
+		private final static Text sText = new Text();
+		private DocnoMapping mDocMapping;
+
+		public void configure(JobConf job) {
+			// load the docid to docno mappings
+			try {
+				mDocMapping = new TrecDocnoMapping();
+
+				// Detect if we're in standalone mode; if so, we can't us the
+				// DistributedCache because it does not (currently) work in
+				// standalone mode...
+				if (job.get("mapred.job.tracker").equals("local")) {
+					FileSystem fs = FileSystem.get(job);
+					String mappingFile = job.get("DocnoMappingFile");
+					mDocMapping.loadMapping(new Path(mappingFile), fs);
+				} else {
+					Path[] localFiles = DistributedCache.getLocalCacheFiles(job);
+					mDocMapping.loadMapping(localFiles[0], FileSystem.getLocal(job));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException("Error initializing DocnoMapping!");
+			}
+		}
+
+		public void map(LongWritable key, TrecDocument doc,
+				OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
+			reporter.incrCounter(Count.DOCS, 1);
+
+			int len = doc.getContent().getBytes().length;
+			sInt.set(mDocMapping.getDocno(doc.getDocid()));
+			sText.set(key + "\t" + len);
+			output.collect(sInt, sText);
+		}
+	}
+
+	public BuildTrecForwardIndex() {
+	}
+
+	private static int printUsage() {
+		System.out.println("usage: [collection-path] [output-path] [index-file] [docno-mapping-file]");
+		ToolRunner.printGenericCommandUsage(System.out);
+		return -1;
+	}
+
+	/**
+	 * Runs this tool.
+	 */
+	public int run(String[] args) throws Exception {
+		if (args.length != 4) {
+			printUsage();
+			return -1;
+		}
+
+		JobConf conf = new JobConf(getConf(), DemoCountTrecDocuments.class);
+		FileSystem fs = FileSystem.get(getConf());
+
+		String collectionPath = args[0];
+		String outputPath = args[1];
+		String indexFile = args[2];
+		String mappingFile = args[3];
+
+		sLogger.info("Tool name: BuildTrecForwardIndex");
+		sLogger.info(" - collection path: " + collectionPath);
+		sLogger.info(" - output path: " + outputPath);
+		sLogger.info(" - index file: " + indexFile);
+		sLogger.info(" - mapping file: " + mappingFile);
+
+		conf.setJobName("BuildTrecForwardIndex");
+
+		conf.set("mapred.child.java.opts", "-Xmx1024m");
+		conf.setNumReduceTasks(1);
+
+		DistributedCache.addCacheFile(new URI(mappingFile), conf);
+
+		FileInputFormat.setInputPaths(conf, new Path(collectionPath));
+		FileOutputFormat.setOutputPath(conf, new Path(outputPath));
+		FileOutputFormat.setCompressOutput(conf, false);
+
+		conf.setInputFormat(TrecDocumentInputFormat.class);
+		conf.setOutputKeyClass(IntWritable.class);
+		conf.setOutputValueClass(Text.class);
+
+		conf.setMapperClass(MyMapper.class);
+		conf.setReducerClass(IdentityReducer.class);
+
+		// delete the output directory if it exists already
+		FileSystem.get(conf).delete(new Path(outputPath), true);
+
+		RunningJob job = JobClient.runJob(conf);
+		
+		Counters counters = job.getCounters();
+		int numDocs = (int) counters.findCounter(Count.DOCS).getCounter();
+
+		String inputFile = outputPath + "/"	+ "part-00000";
+
+		sLogger.info("Writing " + numDocs + " doc offseta to " + indexFile);
+		FSLineReader reader = new FSLineReader(inputFile, fs);
+
+		FSDataOutputStream writer = fs.create(new Path(indexFile), true);
+
+		writer.writeUTF("edu.umd.cloud9.collection.trec.TrecForwardIndex");
+		writer.writeUTF(collectionPath);
+		writer.writeInt(numDocs);
+
+		int cnt = 0;
+		Text line = new Text();
+		while (reader.readLine(line) > 0) {
+			String[] arr = line.toString().split("\\t");
+			long offset = Long.parseLong(arr[1]);
+			int len = Integer.parseInt(arr[2]);
+
+			// sLogger.info(arr[0] + " " + offset + " " + len);
+			writer.writeLong(offset);
+			writer.writeInt(len);
+
+			cnt++;
+			if (cnt % 100000 == 0) {
+				sLogger.info(cnt + " docs");
+			}
+		}
+		reader.close();
+		writer.close();
+		sLogger.info(cnt + " docs total. Done!");
+
+		if (numDocs != cnt) {
+			throw new RuntimeException("Unexpected number of documents in building forward index!");
+		}
+		
+		return 0;
+	}
+
+	/**
+	 * Dispatches command-line arguments to the tool via the
+	 * <code>ToolRunner</code>.
+	 */
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		int res = ToolRunner.run(conf, new BuildTrecForwardIndex(), args);
+		System.exit(res);
+	}
+}
---
+++ src/dist/edu/umd/cloud9/collection/trec/TrecForwardIndex.java
@@ -0,0 +1,104 @@
+package edu.umd.cloud9.collection.trec;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.log4j.Logger;
+
+import edu.umd.cloud9.collection.DocumentForwardIndex;
+
+/**
+ * <p>
+ * Object representing a document forward index for TREC collections.
+ * </p>
+ * 
+ * @author Jimmy Lin
+ * 
+ */
+public class TrecForwardIndex implements DocumentForwardIndex<TrecDocument> {
+
+	private static final Logger sLogger = Logger.getLogger(TrecForwardIndex.class);
+
+	private long[] mOffsets;
+	private int[] mLengths;
+	private FSDataInputStream mCollectionStream;
+	private TrecDocnoMapping mDocnoMapping = new TrecDocnoMapping();
+	private String mCollectionPath;
+
+	public int getDocno(String docid) {
+		return mDocnoMapping.getDocno(docid);
+	}
+
+	public String getDocid(int docno) {
+		return mDocnoMapping.getDocid(docno);
+	}
+
+	public int getLastDocno() {
+		return mOffsets.length-1;
+	}
+
+	public int getFirstDocno() {
+		return 1;
+	}
+
+	public String getCollectionPath() {
+		return mCollectionPath;
+	}
+
+	public String getContentType() {
+		return "text/plain";
+	}
+
+	public TrecDocument getDocument(String docid) {
+		return getDocument(mDocnoMapping.getDocno(docid));
+	}
+
+	public TrecDocument getDocument(int docno) {
+		TrecDocument doc = new TrecDocument();
+
+		try {
+			sLogger.info("docno " + docno + ": byte offset " + mOffsets[docno] + ", length "
+					+ mLengths[docno]);
+
+			mCollectionStream.seek(mOffsets[docno]);
+
+			byte[] arr = new byte[mLengths[docno]];
+
+			mCollectionStream.read(arr);
+
+			TrecDocument.readDocument(doc, new String(arr));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return doc;
+	}
+
+	public void loadIndex(String indexFile, String mappingDataFile) throws IOException {
+		Path p = new Path(indexFile);
+		FileSystem fs = FileSystem.get(new Configuration());
+		FSDataInputStream in = fs.open(p);
+
+		// read and throw away
+		in.readUTF();
+		mCollectionPath = in.readUTF();
+
+		// docnos start at one, so we need an array that's one larger than
+		// number of docs
+		int sz = in.readInt() + 1;
+		mOffsets = new long[sz];
+		mLengths = new int[sz];
+
+		for (int i = 1; i < sz; i++) {
+			mOffsets[i] = in.readLong();
+			mLengths[i] = in.readInt();
+		}
+		in.close();
+
+		mCollectionStream = fs.open(new Path(mCollectionPath));
+		mDocnoMapping.loadMapping(new Path(mappingDataFile), fs);
+	}
+}
