4c982efdbc96af3c6055d637f55bfbfb16d76349
Jordan Boyd-Graber <jbg@TROTSKY-LINUX.(none)>
2011-01-21 20:31:24 -0500

    Adding stub classes

---
+++ src/dist/edu/umd/cloud9/example/translation/TransProbReducer.java
@@ -0,0 +1,52 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package edu.umd.cloud9.example.translation;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+
+import edu.umd.cloud9.io.PairOfStrings;
+
+// reducer: sums up all the counts
+public class TransProbReducer extends Reducer<PairOfStrings, FloatWritable, 
+       PairOfStrings, FloatWritable> {
+
+    @Override
+    public void reduce(PairOfStrings key, Iterable<FloatWritable> values,
+		       Context context) throws 
+                       IOException, InterruptedException {
+
+    }
+}
+    
---
+++ src/dist/edu/umd/cloud9/example/translation/TransProbReducerTest.java
@@ -0,0 +1,89 @@
+
+package edu.umd.cloud9.example.translation;
+
+import static org.apache.hadoop.mrunit.testutil.ExtendedAssert.assertListEquals;
+ 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+ 
+import junit.framework.TestCase;
+ 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.apache.hadoop.mrunit.mock.MockInputSplit;
+import org.apache.hadoop.mrunit.types.Pair;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.umd.cloud9.io.PairOfStrings;
+
+import edu.umd.cloud9.example.translation.TranslationProbability.*;
+ 
+/**
+ * Test cases for the inverted index mapper.
+ */
+public class TransProbReducerTest extends TestCase {
+ 
+    private Reducer<PairOfStrings, FloatWritable, PairOfStrings, FloatWritable> reducer;
+    private ReduceDriver<PairOfStrings, FloatWritable, PairOfStrings, FloatWritable> driver;
+ 
+    /** We expect pathname@offset for the key from each of these */
+    private final FloatWritable EXPECTED_COUNT = new FloatWritable(1);
+ 
+    @Before
+    public void setUp() {
+        reducer = new TransProbReducer();
+        driver = new ReduceDriver<PairOfStrings, FloatWritable, PairOfStrings, FloatWritable>(reducer);
+    }
+
+    @Test
+    public void testOnePair() {
+        List<Pair<PairOfStrings, FloatWritable>> out0 = null;
+        List<Pair<PairOfStrings, FloatWritable>> out1 = null;
+        List<Pair<PairOfStrings, FloatWritable>> out2 = null;
+ 
+        try {
+	    List<FloatWritable> input_values = new ArrayList<FloatWritable>();
+
+	    input_values.add(new FloatWritable(6.0f));
+	    driver.withInputKey(new PairOfStrings("evil", "*"))
+		.setInputValues(input_values);
+	    out0 = driver.run();
+
+
+	    input_values.clear();
+	    input_values.add(new FloatWritable(4.0f));
+
+	    driver.withInputKey(new PairOfStrings("evil", "mal"))
+		.setInputValues(input_values);
+	    out1 = driver.run();
+
+	    input_values.clear();
+	    input_values.add(new FloatWritable(1.0f));
+	    input_values.add(new FloatWritable(1.0f));
+
+	    driver.withInputKey(new PairOfStrings("evil", "malfaisant")).setInputValues(input_values);
+	    out2 = driver.run();
+
+        } catch (IOException ioe) {
+            fail();
+        }
+ 
+        List<Pair<PairOfStrings, FloatWritable>> expected = new ArrayList<Pair<PairOfStrings, FloatWritable>>();
+        expected.add(new Pair<PairOfStrings, FloatWritable>(new PairOfStrings("evil", "mal"), new FloatWritable(2.0f / 3.0f)));
+ 
+        assertListEquals(expected, out1);
+
+        expected = new ArrayList<Pair<PairOfStrings, FloatWritable>>();
+ 
+        assertListEquals(expected, out0);
+
+	expected.add(new Pair<PairOfStrings, FloatWritable>(new PairOfStrings("evil", "malfaisant"), new FloatWritable(1.0f / 3.0f)));
+	assertListEquals(expected, out2);
+    }
+
+}
