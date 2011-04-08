aeb97eb3a37a2565d85e44ce7f31a3c8b043b264
Ke Zhai <kzhai@umd.edu>
2011-04-08 18:03:57 -0400

    add in new data structures

--- src/dist/edu/umd/cloud9/io/array/ArrayOfFloatsWritable.java
+++ src/dist/edu/umd/cloud9/io/array/ArrayOfFloatsWritable.java
@@ -1,32 +1,41 @@
 package edu.umd.cloud9.io.array;
+
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
 import org.apache.hadoop.io.Writable;
 
-
 /**
- * 	An array of floats that implements Writable class.
+ * An array of floats that implements Writable class.
  * 
- * @author ferhanture
+ * @author Ferhan Ture
  */
 public class ArrayOfFloatsWritable implements Writable {
 	float[] array;
-	
-	
+
 	/**
-	 * 	Constructor with no arguments.
+	 * Constructor with no arguments.
 	 */
 	public ArrayOfFloatsWritable() {
 		super();
 	}
 
-	
+	/**
+	 * Constructor take in a one-dimensional array.
+	 * 
+	 * @param array
+	 *            input float array
+	 */
+	public ArrayOfFloatsWritable(float[] array) {
+		this.array = array;
+	}
+
 	/**
 	 * Constructor that takes the size of the array as an argument.
+	 * 
 	 * @param size
-	 * 		number of floats in array
+	 *            number of floats in array
 	 */
 	public ArrayOfFloatsWritable(int size) {
 		super();
@@ -36,58 +45,74 @@ public class ArrayOfFloatsWritable implements Writable {
 	public void readFields(DataInput in) throws IOException {
 		int size = in.readInt();
 		array = new float[size];
-		for(int i=0;i<size;i++){
+		for (int i = 0; i < size; i++) {
 			set(i, in.readFloat());
 		}
 	}
 
 	public void write(DataOutput out) throws IOException {
 		out.writeInt(size());
-		for(int i=0;i<size();i++){
+		for (int i = 0; i < size(); i++) {
 			out.writeFloat(get(i));
 		}
 	}
-	
+
+	/**
+	 * Get a deep copy of the array.
+	 * 
+	 * @return a clone of the array
+	 */
+	public float[] getClone() {
+		return array.clone();
+	}
+
+	/**
+	 * Get a shallow copy of the array.
+	 * 
+	 * @return a pointer to the array
+	 */
+	public float[] getArray() {
+		return array;
+	}
+
 	/**
 	 * Returns the float value at position <i>i</i>.
 	 * 
 	 * @param i
-	 * 		index of float to be returned
-	 * @return
-	 * 		float value at position <i>i</i>
+	 *            index of float to be returned
+	 * @return float value at position <i>i</i>
 	 */
-	public float get(int i){
+	public float get(int i) {
 		return array[i];
 	}
-	
-	
+
 	/**
 	 * Sets the float at position <i>i</i> to <i>f</i>.
+	 * 
 	 * @param i
-	 * 		position in array
+	 *            position in array
 	 * @param f
-	 * 		float value to be set
+	 *            float value to be set
 	 */
-	public void set(int i, float f){
+	public void set(int i, float f) {
 		array[i] = f;
 	}
 
-	
 	/**
 	 * Returns the size of the float array.
-	 * @return
-	 * 		size of array
+	 * 
+	 * @return size of array
 	 */
 	public int size() {
 		return array.length;
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		String s = "[";
-		for(int i=0;i<size();i++){
-			s+=get(i)+",";
+		for (int i = 0; i < size(); i++) {
+			s += get(i) + ",";
 		}
-		s+="]";
+		s += "]";
 		return s;
 	}
 }
