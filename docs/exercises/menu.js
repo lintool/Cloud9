bf487de1df4d8d3929057574da32f7b52a6b7bca
lintool <jimmylin@umd.edu>
2012-12-30 12:02:48 -0500

    Beginning to revise docs and upgrade to bootstrap.

--- docs/exercises/menu.js
+++
@@ -1,7 +0,0 @@
-document.write('<ul id="menus">');
-document.write('  <li class="page_item"><a class="home" title="Home" href="../../index.html">Home</a></li>');
-document.write('  <li class="page_item"><a href="../api/index.html" title="API">API</a></li>');
-document.write('  <li class="page_item"><a href="./index.html" title="Exercises">Exercises</a></li>');
-document.write('  <li class="page_item"><a href="../content/patterns.html" title="Design Patterns &amp; Algorithms">Design Patterns &amp; Algorithms</a></li>');
-document.write('</ul>');
-
