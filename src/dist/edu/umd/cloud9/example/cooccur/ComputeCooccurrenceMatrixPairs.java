c57dfc13b42f20d72f282f14de304618592257b4
lintool <jimmylin@umd.edu>
2013-01-31 12:17:56 -0500

    Refactored pairs and strips impl.

--- src/dist/edu/umd/cloud9/example/cooccur/ComputeCooccurrenceMatrixPairs.java
+++ src/dist/edu/umd/cloud9/example/cooccur/ComputeCooccurrenceMatrixPairs.java
@@ -1,11 +1,11 @@
 /*
- * Cloud9: A MapReduce Library for Hadoop
- * 
+ * Cloud9: A Hadoop toolkit for working with big data
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you
  * may not use this file except in compliance with the License. You may
  * obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0 
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,6 +19,13 @@ package edu.umd.cloud9.example.cooccur;
 import java.io.IOException;
 import java.util.Iterator;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -27,6 +34,7 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -34,170 +42,185 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
+import cern.colt.Arrays;
 import edu.umd.cloud9.io.pair.PairOfStrings;
 
 /**
  * <p>
- * Implementation of the "pairs" algorithm for computing co-occurrence matrices
- * from a large text collection. This algorithm is described in Chapter 3 of
- * "Data-Intensive Text Processing with MapReduce" by Lin &amp; Dyer, as well as
- * the following paper:
+ * Implementation of the "pairs" algorithm for computing co-occurrence matrices from a large text
+ * collection. This algorithm is described in Chapter 3 of "Data-Intensive Text Processing with 
+ * MapReduce" by Lin &amp; Dyer, as well as the following paper:
  * </p>
- * 
- * <blockquote>Jimmy Lin. <b>Scalable Language Processing Algorithms for the
- * Masses: A Case Study in Computing Word Co-occurrence Matrices with
- * MapReduce.</b>
- * <i>Proceedings of the 2008 Conference on Empirical Methods in Natural
- * Language Processing (EMNLP 2008)</i>, pages 419-428.</blockquote>
- * 
- * <p>
- * This program takes the following command-line arguments:
- * </p>
- * 
- * <ul>
- * <li>[input-path]</li>
- * <li>[output-path]</li>
- * <li>[window]</li>
- * <li>[num-reducers]</li>
- * </ul>
- * 
+ *
+ * <blockquote>Jimmy Lin. <b>Scalable Language Processing Algorithms for the Masses: A Case Study in
+ * Computing Word Co-occurrence Matrices with MapReduce.</b> <i>Proceedings of the 2008 Conference
+ * on Empirical Methods in Natural Language Processing (EMNLP 2008)</i>, pages 419-428.</blockquote>
+ *
  * @author Jimmy Lin
  */
 public class ComputeCooccurrenceMatrixPairs extends Configured implements Tool {
-	private static final Logger sLogger = Logger.getLogger(ComputeCooccurrenceMatrixPairs.class);
+  private static final Logger LOG = Logger.getLogger(ComputeCooccurrenceMatrixPairs.class);
 
-	private static class MyMapper extends Mapper<LongWritable, Text, PairOfStrings, IntWritable> {
+  private static class MyMapper extends Mapper<LongWritable, Text, PairOfStrings, IntWritable> {
+    private static final PairOfStrings PAIR = new PairOfStrings();
+    private static final IntWritable ONE = new IntWritable(1);
+    private int window = 2;
 
-		private final PairOfStrings pair = new PairOfStrings();
-		private final IntWritable one = new IntWritable(1);
-		private int window = 2;
+    @Override
+    public void setup(Context context) {
+      window = context.getConfiguration().getInt("window", 2);
+    }
 
-		@Override
-		public void setup(Context context) {
-			window = context.getConfiguration().getInt("window", 2);
-		}
+    @Override
+    public void map(LongWritable key, Text line, Context context)
+        throws IOException, InterruptedException {
+      String text = line.toString();
 
-		@Override
-		public void map(LongWritable key, Text line, Context context) throws IOException,
-				InterruptedException {
-			String text = line.toString();
+      String[] terms = text.split("\\s+");
 
-			String[] terms = text.split("\\s+");
+      for (int i = 0; i < terms.length; i++) {
+        String term = terms[i];
 
-			for (int i = 0; i < terms.length; i++) {
-				String term = terms[i];
+        // skip empty tokens
+        if (term.length() == 0)
+          continue;
 
-				// skip empty tokens
-				if (term.length() == 0)
-					continue;
+        for (int j = i - window; j < i + window + 1; j++) {
+          if (j == i || j < 0)
+            continue;
 
-				for (int j = i - window; j < i + window + 1; j++) {
-					if (j == i || j < 0)
-						continue;
+          if (j >= terms.length)
+            break;
 
-					if (j >= terms.length)
-						break;
+          // skip empty tokens
+          if (terms[j].length() == 0)
+            continue;
 
-					// skip empty tokens
-					if (terms[j].length() == 0)
-						continue;
+          PAIR.set(term, terms[j]);
+          context.write(PAIR, ONE);
+        }
+      }
+    }
+  }
 
-					pair.set(term, terms[j]);
-					context.write(pair, one);
-				}
-			}
-		}
-	}
+  private static class MyReducer extends
+      Reducer<PairOfStrings, IntWritable, PairOfStrings, IntWritable> {
+    private final static IntWritable SUM = new IntWritable();
 
-	private static class MyReducer extends
-			Reducer<PairOfStrings, IntWritable, PairOfStrings, IntWritable> {
+    @Override
+    public void reduce(PairOfStrings key, Iterable<IntWritable> values, Context context)
+        throws IOException, InterruptedException {
+      Iterator<IntWritable> iter = values.iterator();
+      int sum = 0;
+      while (iter.hasNext()) {
+        sum += iter.next().get();
+      }
 
-		private final static IntWritable SumValue = new IntWritable();
+      SUM.set(sum);
+      context.write(key, SUM);
+    }
+  }
 
-		@Override
-		public void reduce(PairOfStrings key, Iterable<IntWritable> values, Context context)
-				throws IOException, InterruptedException {
-			Iterator<IntWritable> iter = values.iterator();
-			int sum = 0;
-			while (iter.hasNext()) {
-				sum += iter.next().get();
-			}
+  protected static class MyPartitioner extends Partitioner<PairOfStrings, IntWritable> {
+    @Override
+    public int getPartition(PairOfStrings key, IntWritable value, int numReduceTasks) {
+      return (key.getLeftElement().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+    }
+  }
 
-			SumValue.set(sum);
-			context.write(key, SumValue);
-		}
-	}
+  /**
+   * Creates an instance of this tool.
+   */
+  public ComputeCooccurrenceMatrixPairs() {}
 
-	/**
-	 * Creates an instance of this tool.
-	 */
-	public ComputeCooccurrenceMatrixPairs() {
-	}
+  private static final String INPUT = "input";
+  private static final String OUTPUT = "output";
+  private static final String WINDOW = "window";
+  private static final String NUM_REDUCERS = "numReducers";
 
-	private static int printUsage() {
-		System.out
-				.println("usage: [input-path] [output-path] [window] [num-reducers]");
-		ToolRunner.printGenericCommandUsage(System.out);
-		return -1;
-	}
+  /**
+   * Runs this tool.
+   */
+  @SuppressWarnings({ "static-access" })
+  public int run(String[] args) throws Exception {
+    Options options = new Options();
 
-	/**
-	 * Runs this tool.
-	 */
-	public int run(String[] args) throws Exception {
-		if (args.length != 4) {
-			printUsage();
-			return -1;
-		}
+    options.addOption(OptionBuilder.withArgName("path").hasArg()
+        .withDescription("input path").create(INPUT));
+    options.addOption(OptionBuilder.withArgName("path").hasArg()
+        .withDescription("output path").create(OUTPUT));
+    options.addOption(OptionBuilder.withArgName("num").hasArg()
+        .withDescription("window size").create(WINDOW));
+    options.addOption(OptionBuilder.withArgName("num").hasArg()
+        .withDescription("number of reducers").create(NUM_REDUCERS));
 
-		String inputPath = args[0];
-		String outputPath = args[1];
+    CommandLine cmdline;
+    CommandLineParser parser = new GnuParser();
 
-		int window = Integer.parseInt(args[2]);
-		int reduceTasks = Integer.parseInt(args[3]);
+    try {
+      cmdline = parser.parse(options, args);
+    } catch (ParseException exp) {
+      System.err.println("Error parsing command line: " + exp.getMessage());
+      return -1;
+    }
 
-		sLogger.info("Tool: ComputeCooccurrenceMatrixPairs");
-		sLogger.info(" - input path: " + inputPath);
-		sLogger.info(" - output path: " + outputPath);
-		sLogger.info(" - window: " + window);
-		sLogger.info(" - number of reducers: " + reduceTasks);
+    if (!cmdline.hasOption(INPUT) || !cmdline.hasOption(OUTPUT)) {
+      System.out.println("args: " + Arrays.toString(args));
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.setWidth(120);
+      formatter.printHelp(this.getClass().getName(), options);
+      ToolRunner.printGenericCommandUsage(System.out);
+      return -1;
+    }
 
-		Job job = new Job(getConf(), "CooccurrenceMatrixPairs");
+    String inputPath = cmdline.getOptionValue(INPUT);
+    String outputPath = cmdline.getOptionValue(OUTPUT);
+    int reduceTasks = cmdline.hasOption(NUM_REDUCERS) ?
+        Integer.parseInt(cmdline.getOptionValue(NUM_REDUCERS)) : 1;
+    int window = cmdline.hasOption(WINDOW) ? Integer.parseInt(cmdline.getOptionValue(WINDOW)) : 2;
 
-		// Delete the output directory if it exists already
-		Path outputDir = new Path(outputPath);
-		FileSystem.get(getConf()).delete(outputDir, true);
+    LOG.info("Tool: " + ComputeCooccurrenceMatrixPairs.class.getSimpleName());
+    LOG.info(" - input path: " + inputPath);
+    LOG.info(" - output path: " + outputPath);
+    LOG.info(" - window: " + window);
+    LOG.info(" - number of reducers: " + reduceTasks);
 
-		job.getConfiguration().setInt("window", window);
+    Job job = Job.getInstance(getConf());
+    job.setJobName(ComputeCooccurrenceMatrixPairs.class.getSimpleName());
+    job.setJarByClass(ComputeCooccurrenceMatrixPairs.class);
 
-		job.setJarByClass(ComputeCooccurrenceMatrixPairs.class);
-		job.setNumReduceTasks(reduceTasks);
+    // Delete the output directory if it exists already.
+    Path outputDir = new Path(outputPath);
+    FileSystem.get(getConf()).delete(outputDir, true);
 
-		FileInputFormat.setInputPaths(job, new Path(inputPath));
-		FileOutputFormat.setOutputPath(job, new Path(outputPath));
+    job.getConfiguration().setInt("window", window);
 
-		job.setMapOutputKeyClass(PairOfStrings.class);
-		job.setMapOutputValueClass(IntWritable.class);
-		job.setOutputKeyClass(PairOfStrings.class);
-		job.setOutputValueClass(IntWritable.class);
+    job.setNumReduceTasks(reduceTasks);
 
-		job.setMapperClass(MyMapper.class);
-		job.setCombinerClass(MyReducer.class);
-		job.setReducerClass(MyReducer.class);
+    FileInputFormat.setInputPaths(job, new Path(inputPath));
+    FileOutputFormat.setOutputPath(job, new Path(outputPath));
 
-		long startTime = System.currentTimeMillis();
-		job.waitForCompletion(true);
-		System.out.println("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
+    job.setMapOutputKeyClass(PairOfStrings.class);
+    job.setMapOutputValueClass(IntWritable.class);
+    job.setOutputKeyClass(PairOfStrings.class);
+    job.setOutputValueClass(IntWritable.class);
 
-		return 0;
-	}
+    job.setMapperClass(MyMapper.class);
+    job.setCombinerClass(MyReducer.class);
+    job.setReducerClass(MyReducer.class);
+    job.setPartitionerClass(MyPartitioner.class);
 
-	/**
-	 * Dispatches command-line arguments to the tool via the
-	 * <code>ToolRunner</code>.
-	 */
-	public static void main(String[] args) throws Exception {
-		int res = ToolRunner.run(new ComputeCooccurrenceMatrixPairs(), args);
-		System.exit(res);
-	}
+    long startTime = System.currentTimeMillis();
+    job.waitForCompletion(true);
+    System.out.println("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
+
+    return 0;
+  }
+
+  /**
+   * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
+   */
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new ComputeCooccurrenceMatrixPairs(), args);
+  }
 }
